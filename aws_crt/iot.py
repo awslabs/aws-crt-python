a92f40ad725d35d6221cede2afee61e26aa9f4b3
Colden Cullen <colden@amazon.com>
2018-11-02 12:44:28 -0700

    Rename aws_crt_python -> aws_crt

--- aws_crt/iot.py
+++ aws_crt/iot.py
@@ -11,7 +11,7 @@
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
 
-from aws_crt_python import io, mqtt
+from aws_crt import io, mqtt
 
 import sys
 import threading
