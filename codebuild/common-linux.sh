01452009473290b36e4104fba669e616d43bc3a6
Colden Cullen <colden@coldencullen.com>
2018-11-02 14:19:53 -0700

    Add CodeBuild scripts (#4)

---
+++ codebuild/common-linux.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+CMAKE_ARGS="$@"
+
+function install_library {
+    git clone https://github.com/awslabs/$1.git
+    cd $1
+
+    if [ -n "$2" ]; then
+        git checkout $2
+    fi
+
+    mkdir build
+    cd build
+
+    cmake -DCMAKE_INSTALL_PREFIX=../../install -DENABLE_SANITIZERS=ON $CMAKE_ARGS ../
+    make install
+
+    cd ../..
+}
+
+cd ../
+
+mkdir install
+
+install_library s2n 98225d7
+install_library aws-c-common
+install_library aws-c-io tls-s2n
+install_library aws-c-mqtt
+
+cd aws-crt-python
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=../../install -DENABLE_SANITIZERS=ON $CMAKE_ARGS ../
+
+make
+
+LSAN_OPTIONS=verbosity=1:log_threads=1 ctest --output-on-failure
+
+cd ..
+
+./cppcheck.sh ../install/include
---
+++ codebuild/common-windows.bat
@@ -0,0 +1,36 @@
+cd ../
+set CMAKE_ARGS=%*
+
+mkdir install
+
+CALL :install_library aws-c-common
+CALL :install_library aws-c-io tls-s2n
+CALL :install_library aws-c-mqtt
+
+cd aws-crt-python
+mkdir build
+cd build
+cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
+cmake --build . --config Release || goto error
+ctest -V || goto error
+
+goto :EOF
+
+:install_library
+git clone https://github.com/awslabs/%~1.git
+cd %~1
+
+if [%~2] == [] GOTO do_build
+git checkout %~2
+
+:do_build
+mkdir build
+cd build
+cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=../../install ../ || goto error
+cmake --build . --target install --config Release || goto error
+cd ../..
+exit /b %errorlevel%
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
