22c46272c59e53ff349c6c73423c38398fb669ee
Justin Boswell <boswej@amazon.com>
2018-12-06 16:07:33 -0800

    Install chocolatey and python before building

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,6 +1,11 @@
 cd ../
 set CMAKE_ARGS=%*
 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+refreshenv
+choco install python3 -y
+refreshenv
+
 mkdir install
 set AWS_C_INSTALL=%cd%\\install
 
