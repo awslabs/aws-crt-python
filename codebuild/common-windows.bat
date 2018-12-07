65152c8286c40cb128a3e00dbd7fd932069d7f22
Justin Boswell <boswej@amazon.com>
2018-12-06 17:31:07 -0800

    Should probably update the windows script to install %PYTHON_PACKAGE%

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -3,7 +3,7 @@ set CMAKE_ARGS=%*
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 refreshenv
-choco install python3 -y
+choco install %PYTHON_PACKAGE% -y
 refreshenv
 
 mkdir install
