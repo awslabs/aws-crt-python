d239f43616e881933d45e8cbcaca99247e9903b7
Justin Boswell <boswej@amazon.com>
2018-12-11 13:52:53 -0800

    added gdbm and made install_from_brew a function

--- codebuild/common-macos.sh
+++ codebuild/common-macos.sh
@@ -11,33 +11,24 @@ CMAKE_ARGS="$@"
 # so it will be cached for future runs. If the cache is ever blown away, this will update
 # the packages as well
 # If the bottles are already in ./packages, then just install them
-pushd ./packages
-# existing openssl is out of date and must be replaced to install python3
-brew uninstall --ignore-dependencies openssl
-if [ ! -e openssl*bottle*.tar.gz ]; then
-    brew install --build-bottle openssl
-    brew bottle --json openssl
-    brew uninstall openssl --ignore-dependencies
-fi
-brew install openssl*bottle*.tar.gz
+function install_from_brew {
+    pushd ./packages
+    # usually the existing package is too old for one of the others, so uninstall
+    # and reinstall from the cache
+    brew uninstall --ignore-dependencies $1
+    if [ ! -e $1*bottle*.tar.gz ]; then
+        brew install --build-bottle $1
+        brew bottle --json $1
+        brew uninstall --ignore-dependencies $1
+    fi
+    brew install $1*bottle*tar.gz
+    popd
+}
 
-if [ ! -e sqlite*bottle*.tar.gz ]; then
-    brew uninstall sqlite --ignore-dependencies
-    brew install --build-bottle sqlite
-    brew bottle --json sqlite
-    brew uninstall sqlite --ignore-dependencies
-fi
-brew install sqlite*bottle*.tar.gz
-
-# must always uninstall python2 if it's there
-brew uninstall python --ignore-dependencies
-if [ ! -e python*bottle*.tar.gz ]; then
-    brew install --build-bottle python3
-    brew bottle --json python3
-    brew uninstall python3 --ignore-dependencies
-fi
-brew install python*bottle*.tar.gz
-popd
+install_from_brew openssl
+install_from_brew gdbm
+install_from_brew sqlite
+install_from_brew python
 
 # build dependencies
 ./build-deps.sh
