507be7a6443018a4348ec221d0d9549c9966ea94
Justin Boswell <boswej@amazon.com>
2018-12-11 23:53:48 -0800

    this windows script was never building

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,28 +1,23 @@
-cd ../
+
 set CMAKE_ARGS=%*
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-refreshenv
 choco install %PYTHON_PACKAGE% -y
-refreshenv
+call RefreshEnv.cmd
 
-mkdir install
-set AWS_C_INSTALL=%cd%\\install
+mkdir build\deps\install
+set AWS_C_INSTALL=%cd%\build\deps\install
 
 CALL :install_library aws-c-common
 CALL :install_library aws-c-io
-CALL :install_library aws-c-mqtt transactional-tree
+CALL :install_library aws-c-mqtt
 
-cd aws-crt-python
-mkdir build
-cd build
-cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=%AWS_C_INSTALL% ../ || goto error
-cmake --build . --config Release || goto error
-ctest -V || goto error
+python3 setup.py build
 
 goto :EOF
 
 :install_library
+pushd build\deps
 git clone https://github.com/awslabs/%~1.git
 cd %~1
 
@@ -30,7 +25,10 @@ if [%~2] == [] GOTO do_build
 git checkout %~2
 
 :do_build
-python3 setup.py build
+cmake %CMAKE_ARGS% -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=%AWS_C_INSTALL% ../ || goto error
+cmake --build . --config Release || goto error
+cmake --build . --target INSTALL || goto error
+popd
 exit /b %errorlevel%
 
 :error
