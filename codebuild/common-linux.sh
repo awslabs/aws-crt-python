e437262429bebf9ad48a7911151496de2b00b0b4
Jonathan M. Henson <henso@amazon.com>
2018-12-21 15:47:39 -0800

    Added submodule stuff to codebuild.

--- codebuild/common-linux.sh
+++ codebuild/common-linux.sh
@@ -5,27 +5,7 @@ set -x
 
 CMAKE_ARGS="$@"
 
-function install_library {
-    git clone https://github.com/awslabs/$1.git
-    pushd $1
-
-    if [ -n "$2" ]; then
-        git checkout $2
-    fi
-
-    mkdir dep-build
-    cd dep-build
-
-    cmake -DCMAKE_INSTALL_PREFIX=$AWS_C_INSTALL $CMAKE_ARGS ../
-    make install
-    popd
-}
-
+git submodule update --init --recursive
 export AWS_C_INSTALL=`pwd`/build/deps/install
-
-# Linux needs s2n
-install_library s2n
-
-./build-deps.sh $CMAKE_ARGS
 
 python3 setup.py build
--- codebuild/common-macos.sh
+++ codebuild/common-macos.sh
@@ -6,32 +6,13 @@ set -e
 set -x
 
 CMAKE_ARGS="$@"
-
-# ensure each required package is installed, if not, make a bottle for brew in ./packages
-# so it will be cached for future runs. If the cache is ever blown away, this will update
-# the packages as well
-# If the bottles are already in ./packages, then just install them
-function install_from_brew {
-    pushd ./packages
-    # usually the existing package is too old for one of the others, so uninstall
-    # and reinstall from the cache
-    brew uninstall --ignore-dependencies $1
-    if [ ! -e $1*bottle*.tar.gz ]; then
-        brew install --build-bottle $1
-        brew bottle --json $1
-        brew uninstall --ignore-dependencies $1
-    fi
-    brew install $1*bottle*tar.gz
-    popd
-}
-
 install_from_brew openssl
 install_from_brew gdbm
 install_from_brew sqlite
 install_from_brew python
 
+git submodule update --init --recursive
 # build dependencies
-./build-deps.sh $CMAKE_ARGS
 export AWS_C_INSTALL=`pwd`/build/deps/install
 
 # build python3 extension
