365d2a2811aed3218b9332b04d729d5902007aa9
Jonathan M. Henson <jonathan.michael.henson@gmail.com>
2018-12-28 12:21:05 -0800

    Fixed windows builds, added support for running 32 bit vm on 64 bit h… (#26)
    * Fixed windows builds, added support for running 32 bit vm on 64 bit host. Everything is static linking now.

--- setup.py
+++ setup.py
@@ -1,9 +1,72 @@
 import setuptools
 import os
 import subprocess
+import platform
 from os import path
 import sys
+    
+def is_64bit():
+    if sys.maxsize > 2**32:
+        return True
 
+    return False
+
+def is_32bit():
+    return is_64bit() == False
+
+def is_arm ():
+    return platform.machine().startswith('arm')
+
+def determine_cross_compile_string():
+    host_arch = platform.machine()
+    if (host_arch == 'AMD64' or host_arch == 'x86_64') and is_32bit() and sys.platform != 'win32':
+        return 'CMAKE_C_FLAGS=-m32'
+ 
+    return ''        
+    
+def determine_generator_string():
+    if sys.platform == 'win32':
+        vswhere_args = ['%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe', '-legacy', '-latest', '-property', 'installationVersion']
+        vswhere_output = subprocess.check_output(vswhere_args, shell=True)
+        
+        vs_version = None
+        
+        if vswhere_output != None:
+            for out in vswhere_output.split():
+                vs_version = out.decode('utf-8')
+
+        if vs_version == None:
+            print('No version of MSVC compiler could be found!')
+            exit(1)
+
+        print('found MSVC compiler version: {}'.format(vs_version))
+        
+        vs_major_version = vs_version.split('.')[0]
+
+        cmake_list_gen_args = ['cmake', '--help']
+        cmake_help_output = subprocess.check_output(cmake_list_gen_args)
+        
+        vs_version_gen_str = None
+        for out in cmake_help_output.splitlines():
+            trimmed_out = out.decode('utf-8').strip()
+            if 'Visual Studio' in trimmed_out and vs_major_version in trimmed_out:
+                vs_version_gen_str = trimmed_out.split('[')[0].strip()       
+        
+        if vs_version_gen_str == None:
+            print('CMake does not recognize an installed version of visual studio on your system.')
+            exit(1)
+            
+        if is_64bit():
+            print('64bit version of python detected, using win64 builds')
+            vs_version_gen_str = vs_version_gen_str + ' Win64'
+
+        vs_version_gen_str = '-G' + vs_version_gen_str  
+        print('Succesfully determined generator as \"{}\"'.format(vs_version_gen_str))      
+        return vs_version_gen_str
+    return ''    
+    
+generator_string = determine_generator_string()
+cross_compile_string = determine_cross_compile_string()
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
 build_dir = os.path.join(current_dir, 'deps_build')
@@ -22,11 +85,11 @@ common_dir = os.path.join(current_dir, 'aws-c-common')
 s2n_dir = os.path.join(current_dir, 's2n')
 io_dir = os.path.join(current_dir, 'aws-c-io')
 mqtt_dir = os.path.join(current_dir, 'aws-c-mqtt')
-common_cmake_args = ['cmake', '-G', 'Ninja', '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=ON', common_dir]
-s2n_cmake_args = ['cmake', '-G', 'Ninja', '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=ON', s2n_dir]
-io_cmake_args = ['cmake', '-G', 'Ninja', '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=ON', io_dir]
-mqtt_cmake_args = ['cmake', '-G', 'Ninja', '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=ON', mqtt_dir]
-build_cmd = ['cmake', '--build', './', '--target', 'install']
+common_cmake_args = ['cmake', generator_string, cross_compile_string, '-DVERSION_LIBS=OFF', '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=OFF', common_dir]
+s2n_cmake_args = ['cmake', generator_string, cross_compile_string, '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=OFF', s2n_dir]
+io_cmake_args = ['cmake', generator_string, cross_compile_string, '-DVERSION_LIBS=OFF', '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=OFF', io_dir]
+mqtt_cmake_args = ['cmake', generator_string, cross_compile_string, '-DVERSION_LIBS=OFF', '-DCMAKE_INSTALL_PREFIX={}'.format(dep_install_path), '-DBUILD_SHARED_LIBS=OFF', mqtt_dir]
+build_cmd = ['cmake', '--build', './', '--config', 'release', '--target', 'install']
 
 common_build_dir = os.path.join(build_dir, 'aws-c-common')
 s2n_build_dir = os.path.join(build_dir, 's2n')
@@ -52,7 +115,7 @@ if not os.path.exists(s2n_build_dir):
 
 os.chdir(s2n_build_dir)
 
-if sys.platform != 'darwin' and sys.platform != 'windows': 
+if sys.platform != 'darwin' and sys.platform != 'win32': 
     ret_code = subprocess.check_call(s2n_cmake_args, stderr=subprocess.STDOUT, shell=shell)
     ret_code = subprocess.check_call(build_cmd, stderr=subprocess.STDOUT, shell=shell)
 
@@ -87,7 +150,7 @@ cflags = []
 ldflags = []
 
 include_dirs = [path.join(dep_install_path, 'include')]
-libraries = aws_c_libs
+libraries = list(aws_c_libs)
 library_dirs = [path.join(dep_install_path, 'lib')]
 extra_objects = []
 
@@ -96,20 +159,10 @@ if compiler_type == 'msvc':
 else:
     cflags += ['-O0', '-Wextra', '-Werror']
 
-if sys.platform == 'linux':
-    include_dirs = ['/usr/local/include'] + include_dirs
-    library_dirs = ['/usr/local/lib'] + library_dirs
-    libraries += ['s2n', 'crypto']
-    try:
-        cflags = [os.environ['CFLAGS']]
-    except:
-        pass
-    try:
-        ldflags = [os.environ['LDFLAGS']]
-    except:
-        pass
-
-if sys.platform == 'darwin':
+if sys.platform == 'win32':
+    #the windows apis being used under the hood. Since we're static linking we have to follow the entire chain down
+    libraries += ['Secur32', 'Crypt32', 'Advapi32', 'BCrypt', 'Kernel32', 'Ws2_32']
+elif sys.platform == 'darwin':
     try:
         cflags = [os.environ['CFLAGS']]
     except:
@@ -122,8 +175,20 @@ if sys.platform == 'darwin':
     include_dirs = ['/usr/local/include'] + include_dirs
     library_dirs = ['/usr/local/lib'] + library_dirs
     extra_objects = [
-        '{}/lib/lib{}.a'.format(os.getenv('AWS_C_INSTALL'), lib) for lib in aws_c_libs]
-    libraries = []
+        '{}/lib/lib{}.a'.format(dep_install_path, lib) for lib in aws_c_libs]
+else:
+    include_dirs = ['/usr/local/include'] + include_dirs
+    library_dirs = ['/usr/local/lib'] + library_dirs
+    libraries += ['s2n', 'crypto']
+    aws_c_libs += ['s2n']
+    try:
+        cflags = [os.environ['CFLAGS']]
+    except:
+        pass
+    try:
+        ldflags = [os.environ['LDFLAGS']]
+    except:
+        pass
 
 os.environ['CFLAGS'] = ' '.join(cflags)
 os.environ['LDFLAGS'] = ' '.join(ldflags)
@@ -150,7 +215,7 @@ _aws_crt_python = setuptools.Extension(
 setuptools.setup(
     name="aws_crt",
     version="0.0.1",
-    author="Example Author",
+    author="Amazon Web Services, Inc",
     author_email="author@example.com",
     description="A common runtime for AWS Python projects",
     url="https://github.com/awslabs/aws-crt-python",
@@ -163,3 +228,4 @@ setuptools.setup(
     ],
     ext_modules = [_aws_crt_python],
 )
+        
