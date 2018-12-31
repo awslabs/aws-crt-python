5ff8a438304e1578668d71ba5713c7ea4a67aaf7
Jonathan M. Henson <henso@amazon.com>
2018-12-31 13:44:34 -0800

    Clean up, everything should work now.

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -3,7 +3,6 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 echo "Installing python version: %* via choco"
 choco install %* -y
-dir "C:\Program Files (x86)\Microsoft Visual Studio"
 call RefreshEnv.cmd
 
 git submodule update --init --recursive
