ad3b414db76527620c655c911d1e4a7bdb2997da
Jonathan M. Henson <henso@amazon.com>
2018-12-31 11:39:13 -0800

    Added echo of env var so we can see what's going wrong.

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -2,6 +2,7 @@
 set CMAKE_ARGS=%*
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+echo "Installing python version: %PYTHON_PACKAGE% via choco"
 choco install %PYTHON_PACKAGE% -y
 call RefreshEnv.cmd
 
