752893bffb02218f996fea4858063806cac4c068
Jonathan M. Henson <henso@amazon.com>
2019-01-29 14:00:24 -0800

    Changed powershell to batch, removed old python since they need newer compilers.

---
+++ continuous-delivery/build-wheels-win64.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\Python35\python.exe" setup.py sdist bdist_wheel
+"C:\Program Files\Python36\python.exe" setup.py sdist bdist_wheel
+"C:\Program Files\Python37\python.exe" setup.py sdist bdist_wheel
+
