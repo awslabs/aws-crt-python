cc6c1340490432c991ba9d118d1b5cbf45623c5c
Jonathan M. Henson <henso@amazon.com>
2019-01-29 12:37:29 -0800

    Fixed syntax error in script.

--- continuous-delivery/build-wheels-win32.ps1
+++ continuous-delivery/build-wheels-win32.ps1
@@ -1,14 +1,14 @@
 Set-PSDebug -Trace 1
-"C:\\Python27 (x86)\\python.exe" setup.py sdist bdist_wheel
-"C:\\Python27 (x86)\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Python33 (x86)\\python.exe" setup.py sdist bdist_wheel
-"C:\\Python33 (x86)\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Python34 (x86)\\python.exe" setup.py sdist bdist_wheel
-"C:\\Python34 (x86)\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Program Files (x86)\\Python35\\python.exe" setup.py sdist bdist_wheel
-"C:\\Program Files (x86)\\Python35\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Program Files (x86)\\Python36\\python.exe" setup.py sdist bdist_wheel
-"C:\\Program Files (x86)\\Python36\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Program Files (x86)\\Python37\\python.exe" setup.py sdist bdist_wheel
-"C:\\Program Files (x86)\\Python37\\pythonw.exe" setup.py sdist bdist_wheel
+"C:\\Python27\ (x86)\\python.exe setup.py sdist bdist_wheel"
+"C:\\Python27\ (x86)\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Python33\ (x86)\\python.exe setup.py sdist bdist_wheel"
+"C:\\Python33\ (x86)\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Python34\ (x86)\\python.exe setup.py sdist bdist_wheel"
+"C:\\Python34\ (x86)\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Program\ Files\ (x86)\\Python35\\python.exe setup.py sdist bdist_wheel"
+"C:\\Program\ Files\ (x86)\\Python35\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Program\ Files\ (x86)\\Python36\\python.exe setup.py sdist bdist_wheel"
+"C:\\Program\ Files\ (x86)\\Python36\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Program\ Files\ (x86)\\Python37\\python.exe setup.py sdist bdist_wheel"
+"C:\\Program\ Files\ (x86)\\Python37\\pythonw.exe setup.py sdist bdist_wheel"
 
--- continuous-delivery/build-wheels-win64.ps1
+++ continuous-delivery/build-wheels-win64.ps1
@@ -1,14 +1,14 @@
 Set-PSDebug -Trace 1
-"C:\\Python27\\python.exe" setup.py sdist bdist_wheel
-"C:\\Python27\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Python33\\python.exe" setup.py sdist bdist_wheel
-"C:\\Python33\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Python34\\python.exe" setup.py sdist bdist_wheel
-"C:\\Python34\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Program Files\\Python35\\python.exe" setup.py sdist bdist_wheel
-"C:\\Program Files\\Python35\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Program Files\\Python36\\python.exe" setup.py sdist bdist_wheel
-"C:\\Program Files\\Python36\\pythonw.exe" setup.py sdist bdist_wheel
-"C:\\Program Files\\Python37\\python.exe" setup.py sdist bdist_wheel
-"C:\\Program Files\\Python37\\pythonw.exe" setup.py sdist bdist_wheel
+"C:\\Python27\\python.exe setup.py sdist bdist_wheel"
+"C:\\Python27\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Python33\\python.exe setup.py sdist bdist_wheel"
+"C:\\Python33\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Python34\\python.exe setup.py sdist bdist_wheel"
+"C:\\Python34\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Program Files\\Python35\\python.exe setup.py sdist bdist_wheel"
+"C:\\Program Files\\Python35\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Program Files\\Python36\\python.exe setup.py sdist bdist_wheel"
+"C:\\Program Files\\Python36\\pythonw.exe setup.py sdist bdist_wheel"
+"C:\\Program Files\\Python37\\python.exe setup.py sdist bdist_wheel"
+"C:\\Program Files\\Python37\\pythonw.exe setup.py sdist bdist_wheel"
 
