adc12f8d0362b7f0d35e8f1cb29e11d6d659e924
Jonathan M. Henson <henso@amazon.com>
2019-01-30 13:01:03 -0800

    More trickery for old python.

--- continuous-delivery/build-wheels-win32.bat
+++ continuous-delivery/build-wheels-win32.bat
@@ -1,4 +1,4 @@
-setx VS100COMNTOOLS %VS140COMNTOOLS%
+set VS100COMNTOOLS %VS140COMNTOOLS%
 "C:\Python27 (x86)\python.exe" setup.py sdist bdist_wheel
 "C:\Python33 (x86)\python.exe" setup.py sdist bdist_wheel
 "C:\Program Files (x86)\Python35-32\python.exe" setup.py sdist bdist_wheel
--- continuous-delivery/build-wheels-win64.bat
+++ continuous-delivery/build-wheels-win64.bat
@@ -1,4 +1,4 @@
-setx VS100COMNTOOLS %VS140COMNTOOLS%
+set VS100COMNTOOLS %VS140COMNTOOLS%
 "C:\Python27\python.exe" setup.py sdist bdist_wheel
 "C:\Python33\python.exe" setup.py sdist bdist_wheel
 "C:\Program Files\Python35\python.exe" setup.py sdist bdist_wheel
