bc6e9888a40ddbd3c2861c586ae138064689d47e
Jonathan M. Henson <jonathan.michael.henson@gmail.com>
2019-01-30 22:19:22 -0800

    Version 2.2 with extra safety check scripts. (#33)
    * Version 2.2 with extra safety check scripts.

---
+++ continuous-delivery/sanity-check-test-pypi.bat
@@ -0,0 +1,14 @@
+FOR /F "delims=" %%A in ('git describe --abbrev^=0') do ( set CURRENT_VERSION=%%A )
+
+"C:\Program Files\Python37\python.exe" -m pip install -i https://testpypi.python.org/simple --user aws-crt==%CURRENT_VERSION% || goto error
+"C:\Program Files\Python37\python.exe" continuous-delivery\test-pip-install.py || goto error
+
+"C:\Python27\python.exe" -m pip install -i https://testpypi.python.org/simple --user aws-crt==%CURRENT_VERSION% || goto error
+"C:\Python27\python.exe" continuous-delivery\test-pip-install.py || goto error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
+
