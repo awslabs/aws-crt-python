02df84f300a2dab63ac477d978eb19ac577e7cc0
Jonathan M. Henson <henso@amazon.com>
2019-01-30 20:55:52 -0800

    Addressed PR comment, kicking off pipeline.

--- continuous-delivery/sanity-check-test-pypi.sh
+++ continuous-delivery/sanity-check-test-pypi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 CURRENT_TAG_VERSION=$(git describe --abbrev=0)
 python3 -m pip install -i https://testpypi.python.org/simple --user aws-crt==$CURRENT_TAG_VERSION
 python3 continuous-delivery/test-pip-install.py
