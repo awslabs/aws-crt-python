3e4ff6d893372beba4d36635271d6098faf8247b
Jonathan M. Henson <henso@amazon.com>
2019-02-01 16:14:35 -0800

    Fixed typo.

--- continuous-delivery/build-wheels-osx.sh
+++ continuous-delivery/build-wheels-osx.sh
@@ -5,8 +5,8 @@ set -e
 /Library/Frameworks/Python.framework/Versions/2.7/bin/python setup.py bdist_wheel
 #note python 3.3 on OSX is a no-go, there's no installer. If you can build python 3.3 from source
 # you can build the wheel, easy-peasy
-/Library/Frameworks/Python.framework/Versions/3.4/bin/python3 seutp.py bdist_wheel
-/Library/Frameworks/Python.framework/Versions/3.5/bin/python3 seutp.py bdist_wheel
-/Library/Frameworks/Python.framework/Versions/3.6/bin/python3 seutp.py bdist_wheel
-/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 seutp.py sdist bdist_wheel
+/Library/Frameworks/Python.framework/Versions/3.4/bin/python3 setup.py bdist_wheel
+/Library/Frameworks/Python.framework/Versions/3.5/bin/python3 setup.py bdist_wheel
+/Library/Frameworks/Python.framework/Versions/3.6/bin/python3 setup.py bdist_wheel
+/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 setup.py sdist bdist_wheel
 #now you just need to run twine (that's in a different script)
