71c0d4b588dbbda3a4284fdd272bc08285d4b8d6
Justin Boswell <boswej@amazon.com>
2019-04-01 16:59:56 -0700

    Disabled pkcs12 on non-apple

--- source/io.c
+++ source/io.c
@@ -160,10 +160,13 @@ PyObject *aws_py_io_client_tls_ctx_new(PyObject *self, PyObject *args) {
     if (certificate_path && private_key_path) {
         aws_tls_ctx_options_init_client_mtls_from_path(&ctx_options, allocator, certificate_path, private_key_path);
     }
+
+#ifdef __APPLE__
     if (pkcs12_path && pkcs12_password) {
         struct aws_byte_cursor password = aws_byte_cursor_from_c_str(pkcs12_password);
         aws_tls_ctx_options_init_client_mtls_pkcs12_from_path(&ctx_options, allocator, pkcs12_path, &password);
     }
+#endif
 
     if (verify_peer != Py_None) {
         ctx_options.verify_peer = verify_peer == Py_True;
