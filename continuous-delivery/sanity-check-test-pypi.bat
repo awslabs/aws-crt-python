58d35c7171d3b419facdf6fe15ce5ca74bedc053
Justin Boswell <boswej@amazon.com>
2019-06-19 12:49:10 -0700

    Switched deployment scripts to use release format tags instead of… (#54)
    * Switched deployment scripts to use any release format tags instead of annotated tags on master
    
    * Added aws-c-compression to manifest
    
    * Disable caching on pip installs

--- continuous-delivery/sanity-check-test-pypi.bat
+++ continuous-delivery/sanity-check-test-pypi.bat
@@ -1,9 +1,10 @@
-FOR /F "delims=" %%A in ('git describe --abbrev^=0') do ( set CURRENT_VERSION=%%A )
+FOR /F "delims=" %%A in ('git describe --tags') do ( set TAG_VERSION=%%A )
+set CURRENT_VERSION=%TAG_VERSION:v=%
 
-"C:\Program Files\Python37\python.exe" -m pip install -i https://testpypi.python.org/simple --user awscrt==%CURRENT_VERSION% || goto error
+"C:\Program Files\Python37\python.exe" -m pip install --no-cache-dir -i https://testpypi.python.org/simple --user awscrt==%CURRENT_VERSION% || goto error
 "C:\Program Files\Python37\python.exe" continuous-delivery\test-pip-install.py || goto error
 
-"C:\Python27\python.exe" -m pip install -i https://testpypi.python.org/simple --user awscrt==%CURRENT_VERSION% || goto error
+"C:\Python27\python.exe" -m pip install --no-cache-dir -i https://testpypi.python.org/simple --user awscrt==%CURRENT_VERSION% || goto error
 "C:\Python27\python.exe" continuous-delivery\test-pip-install.py || goto error
 
 goto :EOF
