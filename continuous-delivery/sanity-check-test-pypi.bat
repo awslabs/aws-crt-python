b2935682d4161bd61597391b06fb99f0c9d0782a
Justin Boswell <boswej@amazon.com>
2019-06-18 17:21:06 -0700

    Switched deployment scripts to use any release format tags instead of annotated tags on master

--- continuous-delivery/sanity-check-test-pypi.bat
+++ continuous-delivery/sanity-check-test-pypi.bat
@@ -1,4 +1,5 @@
-FOR /F "delims=" %%A in ('git describe --abbrev^=0') do ( set CURRENT_VERSION=%%A )
+FOR /F "delims=" %%A in ('$(git describe --tags') do ( set TAG_VERSION=%%A )
+set CURRENT_VERSION=%TAG_VERSION:v=%
 
 "C:\Program Files\Python37\python.exe" -m pip install -i https://testpypi.python.org/simple --user awscrt==%CURRENT_VERSION% || goto error
 "C:\Program Files\Python37\python.exe" continuous-delivery\test-pip-install.py || goto error
--- continuous-delivery/sanity-check-test-pypi.sh
+++ continuous-delivery/sanity-check-test-pypi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-CURRENT_TAG_VERSION=$(git describe --abbrev=0)
+CURRENT_TAG_VERSION=$(git describe --tags | cut -f2 -dv)
 python3 -m pip install -i https://testpypi.python.org/simple --user awscrt==$CURRENT_TAG_VERSION
 python3 continuous-delivery/test-pip-install.py
 
