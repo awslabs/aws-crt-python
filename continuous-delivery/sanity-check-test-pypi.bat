159dd800cdf48fda6c275dcd16aff2c81f1c9876
Justin Boswell <boswej@amazon.com>
2019-06-19 09:39:28 -0700

    booo copy pasta

--- continuous-delivery/sanity-check-test-pypi.bat
+++ continuous-delivery/sanity-check-test-pypi.bat
@@ -1,4 +1,4 @@
-FOR /F "delims=" %%A in ('$(git describe --tags') do ( set TAG_VERSION=%%A )
+FOR /F "delims=" %%A in ('git describe --tags') do ( set TAG_VERSION=%%A )
 set CURRENT_VERSION=%TAG_VERSION:v=%
 
 "C:\Program Files\Python37\python.exe" -m pip install -i https://testpypi.python.org/simple --user awscrt==%CURRENT_VERSION% || goto error
