b0f8a9f689ad0c7fca0c4266c9a7ccb6ae008039
Graeb <graebm@amazon.com>
2019-09-16 13:25:55 -0700

    detect choco errors

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -2,10 +2,10 @@
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 echo "Installing python version: %* via choco"
-choco install %* -y --no-progress
+choco install %* -y --no-progress || goto error
 call RefreshEnv.cmd
 
-git submodule update --init --recursive
+git submodule update --init --recursive || goto error
 mkdir build\deps\install
 set AWS_C_INSTALL=%cd%\build\deps\install
 
