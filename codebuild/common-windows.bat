9e26fa4ad2ee0090d6f559a7daa41321ff3be3a3
Graeb <graebm@amazon.com>
2019-09-17 16:32:17 -0700

    get unittests passing

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -1,5 +1,3 @@
-@echo on
-
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 echo "Installing python version: %* via choco"
 choco install %* -y --no-progress || goto error
@@ -9,12 +7,11 @@ git submodule update --init --recursive || goto error
 mkdir build\deps\install
 set AWS_C_INSTALL=%cd%\build\deps\install
 
-echo --- installing crt ---
+echo --- installing awscrt ---
 python setup.py build install || goto error
 
-
 echo --- unittest ---
-python -u -v -v -m unittest test.test_http_client -v || goto error
+python -u -v -v -m unittest discover -v || goto error
 
 echo --- elasticurl GET ---
 python elasticurl.py -v ERROR -i https://example.com || goto error
