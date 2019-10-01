df7f0f133f07f96ced85abe9da927faa515ca8f8
Michael Graeb <graebm@amazon.com>
2019-10-01 13:16:34 -0700

    pass aws-libs as 'extra_objects' on macos

--- setup.py
+++ setup.py
@@ -113,21 +113,26 @@ class AwsLib(object):
 
 # The extension depends on these libs.
 # They're built along with the extension, in the order listed.
-aws_libs = []
+AWS_LIBS = []
 if sys.platform != 'darwin' and sys.platform != 'win32':
-    aws_libs.append(AwsLib('s2n', extra_cmake_args=['-DUSE_S2N_PQ_CRYPTO=OFF']))
-aws_libs.append(AwsLib('aws-c-common'))
-aws_libs.append(AwsLib('aws-c-io'))
-aws_libs.append(AwsLib('aws-c-cal'))
-aws_libs.append(AwsLib('aws-c-compression'))
-aws_libs.append(AwsLib('aws-c-http'))
-aws_libs.append(AwsLib('aws-c-mqtt'))
+    AWS_LIBS.append(AwsLib('s2n', extra_cmake_args=['-DUSE_S2N_PQ_CRYPTO=OFF']))
+AWS_LIBS.append(AwsLib('aws-c-common'))
+AWS_LIBS.append(AwsLib('aws-c-io'))
+AWS_LIBS.append(AwsLib('aws-c-cal'))
+AWS_LIBS.append(AwsLib('aws-c-compression'))
+AWS_LIBS.append(AwsLib('aws-c-http'))
+AWS_LIBS.append(AwsLib('aws-c-mqtt'))
+
+
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+DEP_BUILD_DIR = os.path.join(PROJECT_DIR, 'build', 'deps')
+DEP_INSTALL_PATH = os.environ.get('AWS_C_INSTALL', os.path.join(DEP_BUILD_DIR, 'install'))
 
 
 class awscrt_build_ext(setuptools.command.build_ext.build_ext):
     def _build_dependency(self, aws_lib):
         prev_cwd = os.getcwd()  # restore cwd at end of function
-        lib_source_dir = os.path.join(self.current_dir, aws_lib.name)
+        lib_source_dir = os.path.join(PROJECT_DIR, aws_lib.name)
 
         # Skip library if it wasn't pulled
         if not os.path.exists(os.path.join(lib_source_dir, 'CMakeLists.txt')):
@@ -135,7 +140,7 @@ class awscrt_build_ext(setuptools.command.build_ext.build_ext):
             return
 
         print("--- Building dependency: {} ---".format(aws_lib.name))
-        lib_build_dir = os.path.join(self.dep_build_dir, aws_lib.name)
+        lib_build_dir = os.path.join(DEP_BUILD_DIR, aws_lib.name)
         if not os.path.exists(lib_build_dir):
             os.makedirs(lib_build_dir)
 
@@ -148,8 +153,8 @@ class awscrt_build_ext(setuptools.command.build_ext.build_ext):
             'cmake',
             determine_generator_string(),
             determine_cross_compile_string(),
-            '-DCMAKE_PREFIX_PATH={}'.format(self.dep_install_path),
-            '-DCMAKE_INSTALL_PREFIX={}'.format(self.dep_install_path),
+            '-DCMAKE_PREFIX_PATH={}'.format(DEP_INSTALL_PATH),
+            '-DCMAKE_INSTALL_PREFIX={}'.format(DEP_INSTALL_PATH),
             '-DBUILD_SHARED_LIBS=OFF',
             '-DCMAKE_BUILD_TYPE={}'.format(build_type),
             '-DBUILD_TESTING=OFF',
@@ -176,24 +181,20 @@ class awscrt_build_ext(setuptools.command.build_ext.build_ext):
 
     def run(self):
         # build dependencies
-        self.current_dir = os.path.dirname(os.path.realpath(__file__))
-        self.dep_build_dir = os.path.join(self.current_dir, self.build_temp, 'deps')
-        self.dep_install_path = os.environ.get('AWS_C_INSTALL', os.path.join(self.dep_build_dir, 'install'))
-
-        for lib in aws_libs:
+        for lib in AWS_LIBS:
             self._build_dependency(lib)
 
         # update paths so awscrt_ext can access dependencies
-        self.include_dirs.append(os.path.join(self.dep_install_path, 'include'))
+        self.include_dirs.append(os.path.join(DEP_INSTALL_PATH, 'include'))
 
         # some platforms (ex: fedora) use /lib64 instead of just /lib
         lib_dir = 'lib'
-        if is_64bit() and os.path.exists(os.path.join(self.dep_install_path, 'lib64')):
+        if is_64bit() and os.path.exists(os.path.join(DEP_INSTALL_PATH, 'lib64')):
             lib_dir = 'lib64'
-        if is_32bit() and os.path.exists(os.path.join(self.dep_install_path, 'lib32')):
+        if is_32bit() and os.path.exists(os.path.join(DEP_INSTALL_PATH, 'lib32')):
             lib_dir = 'lib32'
 
-        self.library_dirs.append(os.path.join(self.dep_install_path, lib_dir))
+        self.library_dirs.append(os.path.join(DEP_INSTALL_PATH, lib_dir))
 
         # continue with normal build_ext.run()
         super(setuptools.command.build_ext.build_ext, self).run()
@@ -203,8 +204,9 @@ def awscrt_ext():
     # fetch the CFLAGS/LDFLAGS from env
     extra_compile_args = os.environ.get('CFLAGS', '').split()
     extra_link_args = os.environ.get('LDFLAGS', '').split()
+    extra_objects = []
 
-    libraries = [x.name for x in aws_libs]
+    libraries = [x.name for x in AWS_LIBS]
 
     # libraries must be passed to the linker with upstream dependencies listed last.
     libraries.reverse()
@@ -216,11 +218,16 @@ def awscrt_ext():
     elif sys.platform == 'darwin':
         extra_link_args += ['-framework', 'Security']
 
-    else: # unix
+        # HACK: Don't understand why, but if AWS_LIBS are linked normally on macos, we get this error:
+        # ImportError: dlopen(_awscrt.cpython-37m-darwin.so, 2): Symbol not found: _aws_byte_cursor_eq_ignore_case
+        # Workaround is to pass them as 'extra_objects' instead of 'libraries'.
+        extra_objects = [os.path.join(DEP_INSTALL_PATH, 'lib', 'lib{}.a'.format(x.name)) for x in AWS_LIBS]
+        libraries = []
+
+    else:  # unix
         # linker will prefer shared libraries over static if it can find both.
         # force linker to choose static one by using using "-l:libcrypto.a" syntax instead of just "-lcrypto".
         libraries += [':libcrypto.a', 'rt']
-
 
     if distutils.ccompiler.get_default_compiler() != 'msvc':
         extra_compile_args += ['-Wextra', '-Werror', '-Wno-strict-aliasing', '-std=gnu99']
@@ -232,6 +239,7 @@ def awscrt_ext():
         sources=glob.glob('source/*.c'),
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
+        extra_objects=extra_objects
     )
 
 
