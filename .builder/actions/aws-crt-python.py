b88b60f781b002811dffe4c548c7f92678d532d8
Justin Boswell <boswej@amazon.com>
2020-02-04 14:42:42 -0800

    Fixed action version of CI

--- .builder/actions/aws-crt-python.py
+++ .builder/actions/aws-crt-python.py
@@ -1,19 +1,19 @@
 
 import Builder
-import os
+import os, sys
 
 class AWSCrtPython(Builder.Action):
     def run(self, env):        
         # Once the virtualenv is set up, we must use that python, so that the venv is used
-        venv_python = os.path.join('.', 'venv', 'bin', 'python')
+        python = sys.executable
 
         actions = [
             ['git', 'submodule', 'update', '--init', '--recursive'],
-            [venv_python, '-m', 'pip', 'install', '--upgrade', 'autopep8', 'boto3'],
-            [venv_python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=build_ext', '--install-option=--include-dirs{openssl_include}', '--install-option=--library-dirs{openssl_lib}'],
-            [venv_python, '-m', 'unittest', 'discover', '--verbose'],
-            [venv_python, 'aws-common-runtime/aws-c-http/integration-testing/http_client_test.py', venv_python, 'elasticurl.py'],
-            [venv_python, '-m', 'autopep8', '--exit-code', '--diff', '--recursive', 'awscrt', 'test', 'setup.py'],
+            [python, '-m', 'pip', 'install', '--upgrade', 'autopep8', 'boto3'],
+            [python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=build_ext', '--install-option=--include-dirs{openssl_include}', '--install-option=--library-dirs{openssl_lib}'],
+            [python, '-m', 'unittest', 'discover', '--verbose'],
+            [python, 'aws-common-runtime/aws-c-http/integration-testing/http_client_test.py', python, 'elasticurl.py'],
+            [python, '-m', 'autopep8', '--exit-code', '--diff', '--recursive', 'awscrt', 'test', 'setup.py'],
         ]
 
         return Builder.Script(actions, name='aws-crt-python')
