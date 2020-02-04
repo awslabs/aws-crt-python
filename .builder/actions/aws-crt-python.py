be091a75e965073f25768b0b084f8abcd0d2ed47
Justin Boswell <boswej@amazon.com>
2020-02-04 14:30:32 -0800

    Added manylinux1 CI build (and CD script as proof of concept)

--- .builder/actions/aws-crt-python.py
+++ .builder/actions/aws-crt-python.py
@@ -9,10 +9,8 @@ class AWSCrtPython(Builder.Action):
 
         actions = [
             ['git', 'submodule', 'update', '--init', '--recursive'],
-            ['sudo', '{python}', '-m', 'pip', 'install', '--upgrade', 'pip', 'setuptools', 'virtualenv'],
-            ['sudo', '{python}', '-m', 'virtualenv', 'venv'],
-            ['sudo', venv_python, '-m', 'pip', 'install', '--upgrade', 'autopep8', 'boto3'],
-            ['sudo', venv_python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=build_ext', '--install-option=--include-dirs{openssl_include}', '--install-option=--library-dirs{openssl_lib}'],
+            [venv_python, '-m', 'pip', 'install', '--upgrade', 'autopep8', 'boto3'],
+            [venv_python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=build_ext', '--install-option=--include-dirs{openssl_include}', '--install-option=--library-dirs{openssl_lib}'],
             [venv_python, '-m', 'unittest', 'discover', '--verbose'],
             [venv_python, 'aws-common-runtime/aws-c-http/integration-testing/http_client_test.py', venv_python, 'elasticurl.py'],
             [venv_python, '-m', 'autopep8', '--exit-code', '--diff', '--recursive', 'awscrt', 'test', 'setup.py'],
---
+++ .builder/actions/manylinux1-ci.py
@@ -0,0 +1,62 @@
+
+import Builder
+import os, sys
+
+pythons = (
+    'cp34-cp34m',
+    'cp35-cp35m',
+    'cp36-cp36m',
+    'cp37-cp37m',
+    'cp27-cp27m',
+    'cp27-cp27mu',
+)
+
+def python_path(version):
+    return '/opt/python/{}/bin/python'.format(version)
+
+def default_python():
+    return sys.executable
+
+class ManyLinux1Package(Builder.Action):
+    def run(self, env):        
+        steps = []
+        for version in pythons:
+            python = python_path(version)
+            actions = [
+                [python, '-m', 'pip', 'install', '--upgrade', 'autopep8', 'boto3'],
+                [python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=sdist', '--install-option=bdist_wheel'],
+                ['auditwheel', 'repair', '--plat', 'manylinux1_x86_64', 'dist/awscrt-*{}-linux_x86_64.whl'.format(python)],
+                
+                [venv_python, 'aws-common-runtime/aws-c-http/integration-testing/http_client_test.py', venv_python, 'elasticurl.py'],
+                [venv_python, '-m', 'autopep8', '--exit-code', '--diff', '--recursive', 'awscrt', 'test', 'setup.py'],
+            ]
+            steps.append(Builder.Script(actions, name=python))
+
+        copy_steps = [
+            ['cp', '-r', 'wheelhouse' '../dist']
+            ['cp', 'dist/*.tar.gz', '../dist/']
+        ]
+            
+        steps += copy_steps
+
+        return Builder.Script(steps, name='manylinux1-package')
+
+class ManyLinux1CI(Builder.Action):
+    def run(self, env):
+        steps = []
+        for version in pythons:
+            python = python_path(version)
+            python3 = default_python()
+            actions = [
+                [python, '-m', 'pip', 'install', '--upgrade',
+                    "--trusted-host", "pypi.org", "--trusted-host", "files.pythonhosted.org",
+                    "pip", "setuptools", "boto3", "autopep8"],
+                [python, '-m', 'pip', 'install', '.',
+                    '--install-option=--verbose', '--install-option=build_ext', '--install-option=--include-dirs{openssl_include}',
+                    '--install-option=--library-dirs{openssl_lib}'],
+                [python3, 'aws-common-runtime/aws-c-http/integration-testing/http_client_test.py', python3, 'elasticurl.py'],
+                [python3, '-m', 'autopep8', '--exit-code', '--diff', '--recursive', 'awscrt', 'test', 'setup.py'],
+            ]
+            steps.append(Builder.Script(actions, name=python))
+
+        return Builder.Script(steps, name='manylinux1-ci')
