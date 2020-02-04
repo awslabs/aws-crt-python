6ca9dc4ee304ab30441304f182f66e856b1b08e4
Justin Boswell <boswej@amazon.com>
2020-02-04 14:40:00 -0800

    Added manylinux1-x86

---
+++ .github/actions/manylinux1-x86/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -ex
+
+python=/opt/python/cp37-cp37m/bin/python
+
+cd $GITHUB_WORKSPACE
+$python -c "from urllib.request import urlretrieve; urlretrieve('https://raw.githubusercontent.com/awslabs/aws-c-common/builder2/codebuild/builder.py', 'builder.py')"
+$python -m virtualenv venv
+python="./venv/bin/python"
+$python builder.py run $1
+
