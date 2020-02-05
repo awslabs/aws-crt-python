41a14d037c4cbb2c46d5970cc1dd6b374e8f00ea
Justin Boswell <boswej@amazon.com>
2020-02-05 10:37:23 -0800

    Moved back to master for builder.py

--- .github/actions/manylinux1-x86/entrypoint.sh
+++ .github/actions/manylinux1-x86/entrypoint.sh
@@ -5,6 +5,6 @@ set -ex
 python=/opt/python/cp37-cp37m/bin/python
 
 cd $GITHUB_WORKSPACE
-$python -c "from urllib.request import urlretrieve; urlretrieve('https://raw.githubusercontent.com/awslabs/aws-c-common/builder2/codebuild/builder.py', 'builder.py')"
+$python -c "from urllib.request import urlretrieve; urlretrieve('https://raw.githubusercontent.com/awslabs/aws-c-common/master/codebuild/builder.py', 'builder.py')"
 $python builder.py --spec manylinux-default-default-linux-x86 run $1
 
