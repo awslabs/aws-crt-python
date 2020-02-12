87d3660adda406af2584446f733ee3c216843bec
Justin Boswell <boswej@amazon.com>
2020-02-11 18:02:31 -0800

    Updated to new builder

--- .builder/actions/aws_crt_python.py
+++ .builder/actions/aws_crt_python.py
@@ -1,6 +1,8 @@
 
 import Builder
-import os, sys
+from spec import current_platform
+import os
+import sys
 
 class InstallPythonReqs(Builder.Action):
     def __init__(self, **kwargs):
@@ -29,9 +31,15 @@ class AWSCrtPython(Builder.Action):
         # Once the virtualenv is set up, we must use that python, so that the venv is used
         python = sys.executable
 
+        install_options = []
+        if 'linux' == current_platform():
+            install_options = [
+                '--install-option=--include-dirs{openssl_include}',
+                '--install-option=--library-dirs{openssl_lib}']
+
         actions = [
             InstallPythonReqs(deps=['autopep8', 'boto3']),
-            [python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=build_ext', '--install-option=--include-dirs{openssl_include}', '--install-option=--library-dirs{openssl_lib}'],
+            [python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=build_ext', *install_options],
             [python, '-m', 'unittest', 'discover', '--verbose'],
             [python, 'aws-common-runtime/aws-c-http/integration-testing/http_client_test.py', python, 'elasticurl.py'],
             [python, '-m', 'autopep8', '--exit-code', '--diff', '--recursive', 'awscrt', 'test', 'setup.py'],
--- .builder/actions/manylinux1-ci.py
+++ .builder/actions/manylinux1-ci.py
@@ -1,10 +1,11 @@
 
 import Builder
-import os, sys
+import os
+import sys
 from aws_crt_python import InstallPythonReqs
 
 pythons = (
-    #'cp34-cp34m',
+    # 'cp34-cp34m',
     'cp35-cp35m',
     'cp36-cp36m',
     'cp37-cp37m',
@@ -12,32 +13,46 @@ pythons = (
     'cp27-cp27mu',
 )
 
+
 def python_path(version):
     return '/opt/python/{}/bin/python'.format(version)
+
 
 def default_python():
     return sys.executable
 
+
 class ManyLinux1Package(Builder.Action):
-    def run(self, env):        
+    def run(self, env):
         steps = []
         for version in pythons:
             python = python_path(version)
-            actions = [
-                InstallPythonReqs(python=python),
-                [python, '-m', 'pip', 'install', '.', '--install-option=--verbose', '--install-option=sdist', '--install-option=bdist_wheel'],
-                ['auditwheel', 'repair', '--plat', 'manylinux1_x86_64', 'dist/awscrt-*{}-linux_x86_64.whl'.format(python)],
-            ]
+            actions = [InstallPythonReqs(python=python),
+                       [python,
+                        '-m',
+                        'pip',
+                        'install',
+                        '.',
+                        '--install-option=--verbose',
+                        '--install-option=sdist',
+                        '--install-option=bdist_wheel'],
+                       ['auditwheel',
+                        'repair',
+                        '--plat',
+                        'manylinux1_x86_64',
+                        'dist/awscrt-*{}-linux_x86_64.whl'.format(python)],
+                       ]
             steps.append(Builder.Script(actions, name=python))
 
         copy_steps = [
             ['cp', '-r', 'wheelhouse' '../dist']
             ['cp', 'dist/*.tar.gz', '../dist/']
         ]
-            
+
         steps += copy_steps
 
         return Builder.Script(steps, name='manylinux1-package')
+
 
 class ManyLinux1CI(Builder.Action):
     def run(self, env):
@@ -46,7 +61,7 @@ class ManyLinux1CI(Builder.Action):
         steps = []
         for version in pythons:
             python = python_path(version)
-            
+
             actions = [
                 InstallPythonReqs(python=python, deps=['boto3'], trust_hosts=True),
                 [python, '-m', 'pip', 'install', '.',
