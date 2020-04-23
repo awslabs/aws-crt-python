df4962589be680d7d46c307990816fa2b3ba5c62
Dengke Tang <dengket@amazon.com>
2020-04-23 14:13:57 -0700

    unittest to hit local server tornado_h2_server

--- test/test_http_client.py
+++ test/test_http_client.py
@@ -19,6 +19,8 @@ from concurrent.futures import Future
 from io import open  # Python2's built-in open() doesn't return a stream
 import os
 import ssl
+import time
+import collections
 from test import NativeResourceTest
 import threading
 import unittest
@@ -34,8 +36,32 @@ except ImportError:
     import SocketServer
     HTTPServer = SocketServer.TCPServer
 
+
+import tornado.gen
+import tornado.ioloop
+import tornado.iostream
+import tornado.tcpserver
+
+from h2.config import H2Configuration
+from h2.connection import H2Connection
+from h2.events import RequestReceived, DataReceived
+
+
 PROXY_HOST = os.environ.get('proxyhost')
 PROXY_PORT = int(os.environ.get('proxyport', '0'))
+
+
+def create_ssl_context(certfile, keyfile):
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    # RFC 7540 Section 9.2: Implementations of HTTP/2 MUST use TLS version 1.2
+    # or higher. Disable TLS 1.1 and lower.
+    ssl_context.options |= (
+        ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1 | ssl.OP_NO_COMPRESSION
+    )
+    ssl_context.set_ciphers("ECDHE+AESGCM")
+    ssl_context.load_cert_chain(certfile=certfile, keyfile=keyfile)
+    ssl_context.set_alpn_protocols(["h2"])
+    return ssl_context
 
 
 class Response(object):
@@ -66,13 +92,93 @@ class TestRequestHandler(SimpleHTTPRequestHandler):
         self.end_headers()
 
 
+class H2Server(tornado.tcpserver.TCPServer):
+
+    @tornado.gen.coroutine
+    def handle_stream(self, stream, address):
+        handler = H2RequestHandler(stream)
+        yield handler.handle()
+
+
+class H2RequestHandler(object):
+
+    def __init__(self, stream):
+        self.stream = stream
+
+        config = H2Configuration(client_side=False)
+        self.conn = H2Connection(config=config)
+
+    @tornado.gen.coroutine
+    def handle(self):
+        self.conn.initiate_connection()
+        yield self.stream.write(self.conn.data_to_send())
+
+        while True:
+            try:
+                data = yield self.stream.read_bytes(65535, partial=True)
+                if not data:
+                    break
+
+                events = self.conn.receive_data(data)
+                for event in events:
+                    if isinstance(event, RequestReceived):
+                        self.request_received(event.headers, event.stream_id)
+                    elif isinstance(event, DataReceived):
+                        self.conn.reset_stream(event.stream_id)
+
+                yield self.stream.write(self.conn.data_to_send())
+
+            except tornado.iostream.StreamClosedError:
+                break
+
+    def request_received(self, headers, stream_id):
+        headers = collections.OrderedDict(headers)
+        #just test for GET request
+        assert headers[b':method'] == b'GET'
+        path = headers[b':path'].lstrip(b'/')
+        full_path = os.path.join('./'.encode('utf-8'), path)
+
+        if not os.path.exists(full_path):
+            response_headers = (
+                (':status', '404'),
+                ('content-length', '0'),
+                ('server', 'twisted-h2'),
+            )
+            self.conn.send_headers(
+                stream_id, response_headers, end_stream=True
+            )
+        else:
+            """
+            No flow-control and max frame involved, please just test a small file
+            """
+            f = open(full_path,'rb')
+            data = f.read()
+            f.close()
+            response_headers = (
+                (':status', '200'),
+                ('content-length', str(len(data))),
+                ('server', 'tornado-h2'),
+            )
+            self.conn.send_headers(stream_id, response_headers)
+            self.conn.send_data(stream_id, data, end_stream=True)
+
+
 class TestClient(NativeResourceTest):
     hostname = 'localhost'
     timeout = 10  # seconds
 
-    def _start_server(self, secure, http_1_0=False):
+    def _start_server(self, secure, http_1_0=False, h2=False):
         # HTTP/1.0 closes the connection at the end of each request
         # HTTP/1.1 will keep the connection alive
+        if h2:
+            ssl_context = create_ssl_context('test/resources/crt.unittests.crt', 'test/resources/crt.unittests.key')
+            self.server = H2Server(ssl_options=ssl_context)
+            self.port = 8080
+            self.server.listen(self.port)
+            self.ioloop = tornado.ioloop.IOLoop.instance()
+            self.server_thread = threading.Thread(target=self.ioloop.start, name='test_server')
+            self.server_thread.start()
+            return
         if http_1_0:
             TestRequestHandler.protocol_version = "HTTP/1.0"
         else:
@@ -92,18 +198,26 @@ class TestClient(NativeResourceTest):
         self.server_thread = threading.Thread(target=self.server.serve_forever, name='test_server')
         self.server_thread.start()
 
-    def _stop_server(self):
+    def _stop_server(self, h2=False):
+        if h2:
+            ioloop = tornado.ioloop.IOLoop.instance()
+            ioloop.add_callback(ioloop.stop)
+            self.server.stop()
+            self.server_thread.join()
+            return
         self.server.shutdown()
         self.server.server_close()
         self.server_thread.join()
 
-    def _new_client_connection(self, secure, proxy_options=None):
+    def _new_client_connection(self, secure, proxy_options=None, h2=False):
         if secure:
             tls_ctx_opt = TlsContextOptions()
             tls_ctx_opt.override_default_trust_store_from_path(None, 'test/resources/ca.crt')
             tls_ctx = ClientTlsContext(tls_ctx_opt)
             tls_conn_opt = tls_ctx.new_connection_options()
             tls_conn_opt.set_server_name(self.hostname)
+            if h2:
+                tls_conn_opt.set_alpn_list(['h2'])
         else:
             tls_conn_opt = None
 
@@ -136,6 +250,19 @@ class TestClient(NativeResourceTest):
 
     def test_connect_https(self):
         self._test_connect(secure=True)
+    
+    def test_connect_h2(self):
+        self._start_server(secure=True, h2=True)
+        connection = self._new_client_connection(secure=True, h2=True)
+        # close connection
+        shutdown_error_from_close_future = connection.close().exception(self.timeout)
+
+        # assert that error was reported via close_future and shutdown callback
+        # error should be None (normal shutdown)
+        self.assertEqual(None, shutdown_error_from_close_future)
+        self.assertFalse(connection.is_open())
+
+        self._stop_server(h2=True)
 
     # The connection should shut itself down cleanly when the GC collects the HttpClientConnection Python object.
     def _test_connection_closes_on_zero_refcount(self, secure):
@@ -160,18 +287,20 @@ class TestClient(NativeResourceTest):
         self._test_connection_closes_on_zero_refcount(secure=True)
 
     # GET request receives this very file from the server. Super meta.
-    def _test_get(self, secure, proxy_options=None):
+    def _test_get(self, secure, proxy_options=None, h2=False):
 
         # Use HTTP/1.0 in proxy tests or server will keep connection with proxy alive
         # and refuse to shut down for 1 minute at the end of each proxy test
         http_1_0 = proxy_options is not None
 
-        self._start_server(secure, http_1_0)
-        connection = self._new_client_connection(secure, proxy_options)
+        self._start_server(secure, http_1_0, h2)
+        connection = self._new_client_connection(secure, proxy_options, h2=h2)
 
-        test_asset_path = 'test/test_http_client.py'
-
-        request = HttpRequest('GET', '/' + test_asset_path)
+        test_asset_path = 'test/resources/crt.unittests.crt'
+        headers = HttpHeaders([
+            ('Host', self.hostname),
+        ])
+        request = HttpRequest('GET', '/' + test_asset_path, headers)
         response = Response()
         stream = connection.request(request, response.on_response, response.on_body)
         stream.activate()
@@ -188,13 +317,16 @@ class TestClient(NativeResourceTest):
 
         self.assertEqual(None, connection.close().exception(self.timeout))
 
-        self._stop_server()
+        self._stop_server(h2)
 
     def test_get_http(self):
         self._test_get(secure=False)
 
     def test_get_https(self):
         self._test_get(secure=True)
+    
+    def test_get_h2(self):
+        self._test_get(secure=True, h2=True)
 
     def _test_shutdown_error(self, secure):
         # Use HTTP/1.0 connection to force a SOCKET_CLOSED error after request completes
