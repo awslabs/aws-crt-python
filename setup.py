6c773c544caf51afe0276d53142399edea86efcc
Michael Graeb <graebm@amazon.com>
2020-05-07 22:15:00 -0700

    version-bump

--- setup.py
+++ setup.py
@@ -244,7 +244,7 @@ def awscrt_ext():
 
 setuptools.setup(
     name="awscrt",
-    version="0.5.14",
+    version="0.5.15",
     author="Amazon Web Services, Inc",
     author_email="aws-sdk-common-runtime@amazon.com",
     description="A common runtime for AWS Python projects",
