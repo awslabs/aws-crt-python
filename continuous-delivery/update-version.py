e18f041a0c8d17189f2eae2a32f16e0a7a3f0f1c
Justin Boswell <boswej@amazon.com>
2020-05-29 19:32:25 +0000

    Need a commit to differentiate 0.5.16 and 0.5.18

--- continuous-delivery/update-version.py
+++ continuous-delivery/update-version.py
@@ -18,5 +18,5 @@ contents = re.sub(r'(\s)version="1\.0\.0-dev",', r'\1version="{}",'.format(versi
 with open(setup_path, 'w') as setup_py:
     setup_py.write(contents)
 
-setup = contents[contents.rfind('setuptools.setup'):]
-print(setup)
+# setup = contents[contents.rfind('setuptools.setup'):]
+# print(setup)
