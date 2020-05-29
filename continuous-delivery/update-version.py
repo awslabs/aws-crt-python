c5e735347fca94b04b58074d7acc080aa690cc78
Justin Boswell <boswej@amazon.com>
2020-05-28 17:24:58 -0700

    Added python script to do version update, called from all platform builds (#158)
    * Added python script to do version update, called from all platform build scripts
    
    * PR Feedback

---
+++ continuous-delivery/update-version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import subprocess
+
+tag = subprocess.check_output(['git', 'describe', '--tags'])
+# strip the leading v
+version = str(tag[1:].strip(), 'utf8')
+setup_path = os.path.join(os.path.dirname(__file__), '..', 'setup.py')
+print("Updating setup.py to version {}".format(version))
+with open(setup_path, 'r+') as setup_py:
+    contents = setup_py.read()
+    contents = re.sub(r'(\s)version=.+,', r'\1version="{}",'.format(version), contents)
+    setup_py.seek(0, os.SEEK_SET)
+    setup_py.write(contents)
