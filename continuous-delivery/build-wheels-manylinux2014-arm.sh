e67a5e21f636e06f68ac8ceed8e0ea23de556c00
Justin Boswell <boswej@amazon.com>
2020-10-12 15:43:43 -0700

    CD fixes (#189)
    * Added logging of file copies to CD jobs
    
    * make bash print commands in CD

--- continuous-delivery/build-wheels-manylinux2014-arm.sh
+++ continuous-delivery/build-wheels-manylinux2014-arm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #before running this, you'll need cmake3 and a compiler. These python versions are just
 #using the default python installers from python.org. Each version needs updated pip, wheel, and setuptools
-set -e
+set -ex
 
 /usr/local/bin/python3.8 ./continuous-delivery/update-version.py
 
@@ -20,6 +20,6 @@ auditwheel repair --plat manylinux2014_aarch64 dist/awscrt-*cp37*.whl
 auditwheel repair --plat manylinux2014_aarch64 dist/awscrt-*cp38*.whl
 
 rm dist/*.whl
-cp -r wheelhouse/* dist/
+cp -rv wheelhouse/* dist/
 
 #now you just need to run twine (that's in a different script)
