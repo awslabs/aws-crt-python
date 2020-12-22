8cba621f81580d900dcf84dfcaf78bd92d5101b9
Dengke Tang <dengket@amazon.com>
2020-12-22 15:42:08 -0800

    not sure about how python deal with or

--- awscrt/s3.py
+++ awscrt/s3.py
@@ -229,7 +229,7 @@ class S3Request(NativeResource):
             region=None):
         assert isinstance(client, S3Client)
         assert isinstance(request, HttpRequest)
-        assert os.path.isfile(file) or file is None
+        assert file is None or os.path.isfile(file)
         # assert file is path or file is None
         assert callable(on_headers) or on_headers is None
         assert callable(on_body) or on_body is None
