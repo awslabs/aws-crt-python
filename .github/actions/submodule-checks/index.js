9b361437c11fa76a25f44a283959a00637f8eee3
Michael Graeb <graebm@amazon.com>
2021-05-12 21:56:26 -0700

    wip

--- .github/actions/submodule-checks/index.js
+++ .github/actions/submodule-checks/index.js
@@ -4,15 +4,15 @@ const exec = require('@actions/exec');
 
 // cwd: optional string
 // check: set false
-const run = async function(args, opts={}) {
+const run = async function (args, opts = {}) {
     let result = {};
     result.stdout = '';
 
     const execOpts = {};
     execOpts.listeners = {
-      stdout: (data) => {
-        result.stdout += data.toString();
-      },
+        stdout: (data) => {
+            result.stdout += data.toString();
+        },
     };
 
     if ('cwd' in opts) {
@@ -24,7 +24,7 @@ const run = async function(args, opts={}) {
 }
 
 // Returns array of submodules, where each item has properties: name, path, url
-const getSubmodules = async function() {
+const getSubmodules = async function () {
     const gitResult = await run(['git', 'config', '--file', '.gitmodules', '--list']);
     // output looks like:
     // submodule.aws-common-runtime/aws-c-common.path=crt/aws-c-common
@@ -61,21 +61,52 @@ const getSubmodules = async function() {
     }
 
     // return array, sorted by name
-    return Object.values(map).sort((a,b) => a.name.localeCompare(b.name));
+    return Object.values(map).sort((a, b) => a.name.localeCompare(b.name));
+}
+
+const diffSubmodule = async function (submodule, targetBranch) {
+    const gitResult = await run(['git', 'diff', targetBranch, '--', submodule.path]);
+    const stdout = gitResult.stdout;
+
+    // output looks like this:
+    //
+    // diff --git a/crt/aws-c-auth b/crt/aws-c-auth
+    // index b6656aa..c74534c 160000
+    // --- a/crt/aws-c-auth
+    // +++ b/crt/aws-c-auth
+    // @@ -1 +1 @@
+    // -Subproject commit b6656aad42edd5d11eea50936cb60359a6338e0b
+    // +Subproject commit c74534c13264868bbbd14b419c291580d3dd9141
+    try {
+        // let's just be naive and only look at the last 2 lines
+        // if this fails in any way, report no difference
+        var result = {}
+        result.targetCommit = stdout.match('\\-Subproject commit ([a-f0-9]{40})')[1];
+        result.sourceCommit = stdout.match('\\+Subproject commit ([a-f0-9]{40})')[1];
+        return result;
+    } catch (error) {
+        return null;
+    }
 }
 
 
-const main = async function() {
-    core.info(`GITHUB_SHA: ${github.context.sha}`)
-    core.info(`GITHUB_REF: ${github.context.ref}`)
-    core.info(`GITHUB_BASE_REF: ${process.env.GITHUB_BASE_REF}`)
-    core.info(`github.head_ref: ${github.head_ref}`)
+const main = async function () {
     const rootDir = process.cwd();
+    console.log(github.context.payload);
+    // TODO: figure out how to access target branch
+    // instead of hardcoding 'main'
+    const targetBranch = 'origin/main';
 
     const submodules = await getSubmodules();
     for (var i = 0; i < submodules.length; i++) {
         const submodule = submodules[i];
-        //diff = await diffSubmodule(submodule);
+
+        // diff submodule against target branch
+        // if there's no difference, there's no need to analyze further
+        diff = await diffSubmodule(submodule, targetBranch);
+        if (diff == null) {
+            continue
+        }
     }
 }
 
