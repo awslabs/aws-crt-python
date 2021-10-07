24bf341d3206cf8aec1644a5437530e1ec1082cf
Dengke Tang <815825145@qq.com>
2021-10-07 09:46:38 -0700

    Support 3.10 wheel (#316)
    * Support 3.10 wheel for all but manylinux1

--- continuous-delivery/build-wheels-win32.bat
+++ continuous-delivery/build-wheels-win32.bat
@@ -5,6 +5,7 @@
 "C:\Program Files (x86)\Python37-32\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files (x86)\Python38-32\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files (x86)\Python39-32\python.exe" setup.py sdist bdist_wheel || goto error
+"C:\Program Files (x86)\Python310-32\python.exe" setup.py sdist bdist_wheel || goto error
 
 goto :EOF
 
--- continuous-delivery/build-wheels-win64.bat
+++ continuous-delivery/build-wheels-win64.bat
@@ -4,6 +4,7 @@
 "C:\Program Files\Python37\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files\Python38\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files\Python39\python.exe" setup.py sdist bdist_wheel || goto error
+"C:\Program Files\Python310\python.exe" setup.py sdist bdist_wheel || goto error
 
 goto :EOF
 
