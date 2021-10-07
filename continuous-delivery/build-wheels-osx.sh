c8b2b24b9feb56f70de47ccce2c4f72b5f705258
Dengke Tang <dengket@amazon.com>
2021-10-07 11:30:16 -0700

    I guess it should be like this

--- continuous-delivery/build-wheels-osx.sh
+++ continuous-delivery/build-wheels-osx.sh
@@ -7,9 +7,9 @@ set -e
 
 export MACOSX_DEPLOYMENT_TARGET="10.9"
 /Library/Frameworks/Python.framework/Versions/3.6/bin/python3 setup.py bdist_wheel
-/Library/Frameworks/Python.framework/Versions/3.7/bin/python3 setup.py sdist bdist_wheel
-/Library/Frameworks/Python.framework/Versions/3.8/bin/python3 setup.py sdist bdist_wheel
-/Library/Frameworks/Python.framework/Versions/3.9/bin/python3 setup.py sdist bdist_wheel
-/Library/Frameworks/Python.framework/Versions/3.10/bin/python3 setup.py sdist bdist_wheel
+# /Library/Frameworks/Python.framework/Versions/3.7/bin/python3 setup.py sdist bdist_wheel
+# /Library/Frameworks/Python.framework/Versions/3.8/bin/python3 setup.py sdist bdist_wheel
+# /Library/Frameworks/Python.framework/Versions/3.9/bin/python3 setup.py sdist bdist_wheel
+# /Library/Frameworks/Python.framework/Versions/3.10/bin/python3 setup.py sdist bdist_wheel
 
 #now you just need to run twine (that's in a different script)
