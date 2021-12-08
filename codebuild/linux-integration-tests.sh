537115611c246e224f2a19c9f0a965fe4dd8c48e
Michael Graeb <graebm@amazon.com>
2021-12-07 17:09:56 -0800

    ensure that linux-integration-tests doesn't skip any tests

--- codebuild/linux-integration-tests.sh
+++ codebuild/linux-integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -euxo pipefail
 
 if test -f "/tmp/setup_proxy_test_env.sh"; then
     source /tmp/setup_proxy_test_env.sh
@@ -15,4 +15,13 @@ cd $CODEBUILD_SRC_DIR
 
 export AWS_TEST_S3=YES
 python -m pip install --verbose .
-python -m unittest discover --failfast --verbose
+python -m unittest discover --failfast --verbose 2>&1 | tee /tmp/tests.log
+
+# ensure no tests were skipped
+if grep -c 'OK (skipped=' /tmp/tests.log
+then
+    echo "ERROR: Some tests were skipped"
+    exit -1
+else
+    echo "No tests skipped"
+fi
