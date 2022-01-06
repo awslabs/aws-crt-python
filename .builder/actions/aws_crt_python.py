963556023e4b7578853f56c3ab87df3aee5f2e61
Michael Graeb <graebm@amazon.com>
2022-01-06 00:33:21 +0000

    Use AWS CLI instead of boto3
    Trying to install boto3, and then import it while the builder was running, didn't work 100% of the time

--- .builder/actions/aws_crt_python.py
+++ .builder/actions/aws_crt_python.py
@@ -1,6 +1,7 @@
 
 import Builder
 import argparse
+import json
 import os.path
 import pathlib
 import subprocess
@@ -11,7 +12,7 @@ import tempfile
 class InstallPythonReqs(Builder.Action):
     def __init__(self, trust_hosts=False, deps=[], python=sys.executable):
         self.trust_hosts = trust_hosts
-        self.core = ('pip', 'setuptools')
+        self.core = ('pip', 'setuptools', 'wheel')
         self.deps = deps
         self.python = python
 
@@ -25,7 +26,8 @@ class InstallPythonReqs(Builder.Action):
         # package database in time for the subsequent install and pip fails
         steps = []
         for deps in (self.core, self.deps):
-            steps.append([self.python, '-m', 'pip', 'install', *trusted_hosts, *deps])
+            if deps:
+                steps.append([self.python, '-m', 'pip', 'install', '--upgrade', *trusted_hosts, *deps])
 
         return Builder.Script(steps, name='install-python-reqs')
 
@@ -33,37 +35,50 @@ class InstallPythonReqs(Builder.Action):
 class SetupForTests(Builder.Action):
 
     def run(self, env):
-        # we want to use boto3 to pull data from secretsmanager
-        # boto3 might need to be installed first...
-        try:
-            import boto3
-        except BaseException:
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'boto3'])
-            import boto3
+        self.env = env
 
-        secrets = boto3.client('secretsmanager')
+        self._setenv_from_secret('AWS_TEST_IOT_MQTT_ENDPOINT', 'unit-test/endpoint')
 
-        # get string from secretsmanager and store in environment variable
-        def setenv_from_secret(env_var_name, secret_name):
-            response = secrets.get_secret_value(SecretId=secret_name)
-            env.shell.setenv(env_var_name, response['SecretString'])
-
-        # get file contents from secretsmanager, store as file under /tmp
-        # and store path in environment variable
-        def setenv_tmpfile_from_secret(env_var_name, secret_name, file_name):
-            response = secrets.get_secret_value(SecretId=secret_name)
-            file_contents = response['SecretString']
-            file_path = os.path.join(tempfile.gettempdir(), file_name)
-            pathlib.Path(file_path).write_text(file_contents)
-            env.shell.setenv(env_var_name, file_path)
-
-        setenv_from_secret('AWS_TEST_IOT_MQTT_ENDPOINT', 'unit-test/endpoint')
-
-        setenv_tmpfile_from_secret('AWS_TEST_TLS_CERT_PATH', 'unit-test/certificate', 'certificate.pem')
-        setenv_tmpfile_from_secret('AWS_TEST_TLS_KEY_PATH', 'unit-test/privatekey', 'privatekey.pem')
+        self._setenv_tmpfile_from_secret('AWS_TEST_TLS_CERT_PATH', 'unit-test/certificate', 'certificate.pem')
+        self._setenv_tmpfile_from_secret('AWS_TEST_TLS_KEY_PATH', 'unit-test/privatekey', 'privatekey.pem')
 
         # enable S3 tests
         env.shell.setenv('AWS_TEST_S3', '1')
+
+    def _get_secret(self, secret_id):
+        """get string from secretsmanager"""
+
+        # NOTE: using AWS CLI instead of boto3 because we know CLI is already
+        # installed wherever builder is run. Once upon a time we tried using
+        # boto3 by installing it while the builder was running but this didn't
+        # work in some rare scenarios.
+
+        cmd = ['aws', 'secretsmanager', 'get-secret-value', '--secret-id', secret_id]
+        # NOTE: print command args, but use "quiet" mode so that output isn't printed.
+        # we don't want secrets leaked to the build log
+        print('>', subprocess.list2cmdline(cmd))
+        result = self.env.shell.exec(*cmd, check=True, quiet=True)
+        secret_value = json.loads(result.output)
+        return secret_value['SecretString']
+
+    def _tmpfile_from_secret(self, secret_name, file_name):
+        """get file contents from secretsmanager, store as file under /tmp, return file path"""
+        file_contents = self._get_secret(secret_name)
+        file_path = os.path.join(tempfile.gettempdir(), file_name)
+        print(f"Writing to: {file_path}")
+        pathlib.Path(file_path).write_text(file_contents)
+        return file_path
+
+    def _setenv_from_secret(self, env_var_name, secret_name):
+        """get string from secretsmanager and store in environment variable"""
+
+        secret_value = self._get_secret(secret_name)
+        self.env.shell.setenv(env_var_name, secret_value)
+
+    def _setenv_tmpfile_from_secret(self, env_var_name, secret_name, file_name):
+        """get file contents from secretsmanager, store as file under /tmp, and store path in environment variable"""
+        file_path = self._tmpfile_from_secret(secret_name, file_name)
+        self.env.shell.setenv(env_var_name, file_path)
 
 
 class AWSCrtPython(Builder.Action):
@@ -76,9 +91,7 @@ class AWSCrtPython(Builder.Action):
         python = args.python if args.python else sys.executable
 
         actions = [
-            [sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip', 'setuptools', 'wheel'],
-            [sys.executable, '-m', 'pip', 'install', '--upgrade', 'boto3'],
-            InstallPythonReqs(deps=['boto3'], python=python),
+            InstallPythonReqs(deps=[], python=python),
             SetupForTests(),
             [python, '-m', 'pip', 'install', '--verbose', '.'],
             # "--failfast" because, given how our leak-detection in tests currently works,
