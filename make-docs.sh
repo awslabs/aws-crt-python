ba558ca2190c128a859fbf91562b5441a8e1a520
Michael Graeb <graebm@amazon.com>
2022-04-14 17:09:11 -0700

    Enable warnings-as-errors for docs Fix warnings

--- make-docs.sh
+++ make-docs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 pushd `dirname $0`/docsrc > /dev/null
-make html
+rm -rf build/
+make html SPHINXOPTS="-W --keep-going"
 rm -rf ../docs/
 cp -a build/html/. ../docs
 touch ../docs/.nojekyll
