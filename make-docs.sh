37803493c062484f97cf381890f4fb7b02f9ec88
Michael Graeb <graebm@amazon.com>
2022-04-15 09:43:46 -0700

    add comments to shell script

--- make-docs.sh
+++ make-docs.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 set -e
-pushd `dirname $0`/docsrc > /dev/null
-rm -rf build/
+
+pushd `dirname $0` > /dev/null
+
+# clean
+rm -rf docs/
+rm -rf docsrc/build/
+
+# build
+pushd docsrc > /dev/null
 make html SPHINXOPTS="-W --keep-going"
-rm -rf ../docs/
-cp -a build/html/. ../docs
-touch ../docs/.nojekyll
+popd > /dev/null
+
+cp -a docsrc/build/html/. docs
+
+# The existence of this file tells GitHub Pages to just host the HTML as-is
+# https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
+touch docs/.nojekyll
+
 popd > /dev/null
