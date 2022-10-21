a1f7cbfb5cdbceead265b7dd1193b89a587d7ebc
Michael Graeb <graebm@amazon.com>
2022-10-21 15:48:26 -0700

    Drop support for Python 3.6, add wheels for Python 3.11

--- continuous-delivery/build-wheels-win32.bat
+++ continuous-delivery/build-wheels-win32.bat
@@ -1,11 +1,11 @@
 
 "C:\Program Files (x86)\Python39-32\python.exe" .\continuous-delivery\update-version.py || goto error
 
-"C:\Program Files (x86)\Python36-32\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files (x86)\Python37-32\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files (x86)\Python38-32\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files (x86)\Python39-32\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files (x86)\Python310-32\python.exe" setup.py sdist bdist_wheel || goto error
+"C:\Program Files (x86)\Python311-32\python.exe" setup.py sdist bdist_wheel || goto error
 
 goto :EOF
 
--- continuous-delivery/build-wheels-win64.bat
+++ continuous-delivery/build-wheels-win64.bat
@@ -1,10 +1,10 @@
 "C:\Program Files\Python39\python.exe" continuous-delivery\update-version.py || goto error
 
-"C:\Program Files\Python36\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files\Python37\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files\Python38\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files\Python39\python.exe" setup.py sdist bdist_wheel || goto error
 "C:\Program Files\Python310\python.exe" setup.py sdist bdist_wheel || goto error
+"C:\Program Files\Python311\python.exe" setup.py sdist bdist_wheel || goto error
 
 goto :EOF
 
