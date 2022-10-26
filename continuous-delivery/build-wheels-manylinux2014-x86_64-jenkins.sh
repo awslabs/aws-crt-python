2149918b59fe8248285668d40886f2db080b98f9
Michael Graeb <graebm@amazon.com>
2022-10-25 17:14:20 -0700

    Use new path to these docker images. aws-crt-builder started uploading to a different path like a year ago and we forget to update it here

--- continuous-delivery/build-wheels-manylinux2014-x86_64-jenkins.sh
+++ continuous-delivery/build-wheels-manylinux2014-x86_64-jenkins.sh
@@ -2,7 +2,7 @@
 #run build-wheels script in manylinux2014 docker image
 set -ex
 
-DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/aws-crt/manylinux2014-x64:latest
+DOCKER_IMAGE=123124136734.dkr.ecr.us-east-1.amazonaws.com/aws-crt-manylinux2014-x64:latest
 
 $(aws --region us-east-1 ecr get-login --no-include-email)
 
