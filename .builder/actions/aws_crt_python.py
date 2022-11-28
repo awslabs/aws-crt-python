09e139bc505fca3d25e46ead90cb92dacda18704
Steve Kim <sbstevek@amazon.com>
2022-11-28 08:48:58 -0800

    run specific test

--- .builder/actions/aws_crt_python.py
+++ .builder/actions/aws_crt_python.py
@@ -253,7 +253,7 @@ class AWSCrtPython(Builder.Action):
             # "--failfast" because, given how our leak-detection in tests currently works,
             # once one test fails all the rest usually fail too.
             # [python, '-m', 'unittest', 'discover', '--verbose', '--failfast'],
-            [python, '-m', 'unittest', '--verbose', '--failfast', 'test.test_mqtt5.Mqtt5ClientTest'],
+            [python, '-m', 'unittest', '--verbose', '--failfast', 'test.test_mqtt5.Mqtt5ClientTest.test_connect_with_invalid_host_name'],
             # http_client_test.py launches external processes using the extra args
             [python, 'crt/aws-c-http/integration-testing/http_client_test.py',
                 python, 'elasticurl.py'],
