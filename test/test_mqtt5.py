32ba788d8906bf4d0cb9c554861f5a8198d97da6
Steve Kim <sbstevek@amazon.com>
2022-11-29 08:41:52 -0800

    added will to empty wills

--- test/test_mqtt5.py
+++ test/test_mqtt5.py
@@ -208,6 +208,7 @@ class Mqtt5ClientTest(NativeResourceTest):
         if client_options.connect_options is None:
             client_options.connect_options = mqtt5.ConnectPacket()
             client_options.connect_options.client_id = create_client_id()
+            client_options.connect_options.will = mqtt5.PublishPacket()
 
         if (auth_type == AuthType.DIRECT or
             auth_type == AuthType.DIRECT_BASIC_AUTH or
