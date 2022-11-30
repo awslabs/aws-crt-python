99a0cf17da6c84c61cd227041def39a95319558b
Steve Kim <sbstevek@amazon.com>
2022-11-29 16:43:30 -0800

    reinstate segfaulting tests

--- test/test_mqtt5.py
+++ test/test_mqtt5.py
@@ -521,11 +521,11 @@ class Mqtt5ClientTest(NativeResourceTest):
     #             NEGATIVE CONNECT TEST CASES
     # ==============================================================
 
-    # def test_connect_with_invalid_host_name(self):
-    #     client_options = mqtt5.ClientOptions("badhost", 1883)
-    #     client, callbacks = self._test_connect_fail(auth_type=AuthType.BAD_HOST, client_options=client_options)
-    #     client.stop()
-    #     callbacks.future_stopped.result(TIMEOUT)
+    def test_connect_with_invalid_host_name(self):
+        client_options = mqtt5.ClientOptions("badhost", 1883)
+        client, callbacks = self._test_connect_fail(auth_type=AuthType.BAD_HOST, client_options=client_options)
+        client.stop()
+        callbacks.future_stopped.result(TIMEOUT)
 
     def test_connect_with_invalid_port(self):
         client_options = mqtt5.ClientOptions("badhost", 444)
@@ -541,13 +541,13 @@ class Mqtt5ClientTest(NativeResourceTest):
         client.stop()
         callbacks.future_stopped.result(TIMEOUT)
 
-    # def test_connect_with_socket_timeout(self):
-    #     client_options = mqtt5.ClientOptions("www.example.com", 81)
-    #     client_options.connack_timeout_ms = 200
-    #     client, callbacks = self._test_connect_fail(
-    #         auth_type=AuthType.NO_APPLICATION, client_options=client_options)
-    #     client.stop()
-    #     callbacks.future_stopped.result(TIMEOUT)
+    def test_connect_with_socket_timeout(self):
+        client_options = mqtt5.ClientOptions("www.example.com", 81)
+        client_options.connack_timeout_ms = 200
+        client, callbacks = self._test_connect_fail(
+            auth_type=AuthType.NO_APPLICATION, client_options=client_options)
+        client.stop()
+        callbacks.future_stopped.result(TIMEOUT)
 
     def test_connect_with_incorrect_basic_authentication_credentials(self):
         client_options = mqtt5.ClientOptions("will be replaced", 0)
