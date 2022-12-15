54241542c4d8b1dedfa4fca8fee1d36182ca6333
Steve Kim <sbstevek@amazon.com>
2022-12-15 10:42:12 -0800

    check enum casting

--- awscrt/mqtt5.py
+++ awscrt/mqtt5.py
@@ -641,6 +641,16 @@ class ClientSessionBehaviorType(IntEnum):
     Session rejoin requires an appropriate non-zero session expiry interval in the client's CONNECT options.
     """
 
+    REJOIN_ALWAYS = 3
+    """
+    Always attempt to rejoin an existing session.  Since the client does not support durable session persistence,
+    this option is not guaranteed to be spec compliant because any unacknowledged qos1 publishes (which are
+    part of the client session state) will not be present on the initial connection.  Until we support
+    durable session resumption, this option is technically spec-breaking, but useful.
+
+    Always rejoin requires an appropriate non-zero session expiry interval in the client's CONNECT options.
+    """
+
 
 class PacketType(IntEnum):
     """MQTT5 Packet Type enumeration
@@ -730,7 +740,6 @@ class PacketType(IntEnum):
     Authentication exchange
     """
 
-
 class PayloadFormatIndicator(IntEnum):
     """Optional property describing a PUBLISH payload's format.
 
@@ -746,6 +755,13 @@ class PayloadFormatIndicator(IntEnum):
     """
     The payload is a well-formed utf-8 string value.
     """
+
+
+def _try_payload_format_indicator(value):
+    try:
+        return PayloadFormatIndicator(value)
+    except Exception:
+        return None
 
 
 class RetainAndHandlingType(IntEnum):
@@ -802,6 +818,7 @@ class ExtendedValidationAndFlowControlOptions(IntEnum):
     """
 
 
+
 class ClientOperationQueueBehaviorType(IntEnum):
     """Controls how disconnects affect the queued and in-progress operations tracked by the client.  Also controls
     how operations are handled while the client is not connected.  In particular, if the client is not connected,
@@ -833,6 +850,7 @@ class ClientOperationQueueBehaviorType(IntEnum):
     """
 
 
+
 class ExponentialBackoffJitterMode(IntEnum):
     """Controls how the reconnect delay is modified in order to smooth out the distribution of reconnection attempt
     timepoints for a large set of reconnecting clients.
@@ -860,6 +878,7 @@ class ExponentialBackoffJitterMode(IntEnum):
     Backoff is taken randomly from the interval between the base backoff
     interval and a scaling (greater than 1) of the current backoff value
     """
+
 
 
 @dataclass
@@ -1399,7 +1418,7 @@ class _ClientCore:
         publish_packet.retain = retain
 
         if payload_format_indicator_exists:
-            publish_packet.payload_format_indicator = PayloadFormatIndicator(payload_format_indicator)
+            publish_packet.payload_format_indicator = _try_payload_format_indicator(payload_format_indicator)
         if message_expiry_interval_sec_exists:
             publish_packet.message_expiry_interval_sec = message_expiry_interval_sec
         if topic_alias_exists:
