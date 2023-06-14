19df8c3eabdaba008cc20bde1aebaf72d9364fe4
Waqar Ahmed Khan <waahm7@gmail.com>
2023-06-14 11:12:26 -0700

    MuslLinux Wheels (#480)

--- continuous-delivery/build-wheels-musllinux-1-1-aarch64.sh
+++ continuous-delivery/build-wheels-musllinux-1-1-aarch64.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-#assumes image based on manylinux2014 + extras (cmake3, libcrypto, etc)
+#assumes image based on musllinux_1_1
 set -ex
 
 /opt/python/cp39-cp39/bin/python ./continuous-delivery/update-version.py
 
 /opt/python/cp37-cp37m/bin/python setup.py sdist bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/awscrt-*cp37*.whl
+auditwheel repair --plat musllinux_1_1_aarch64 dist/awscrt-*cp37*.whl
 
 /opt/python/cp38-cp38/bin/python setup.py sdist bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/awscrt-*cp38*.whl
+auditwheel repair --plat musllinux_1_1_aarch64 dist/awscrt-*cp38*.whl
 
 /opt/python/cp39-cp39/bin/python setup.py sdist bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/awscrt-*cp39*.whl
+auditwheel repair --plat musllinux_1_1_aarch64 dist/awscrt-*cp39*.whl
 
 /opt/python/cp310-cp310/bin/python setup.py sdist bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/awscrt-*cp310*.whl
+auditwheel repair --plat musllinux_1_1_aarch64 dist/awscrt-*cp310*.whl
 
 /opt/python/cp311-cp311/bin/python setup.py sdist bdist_wheel
-auditwheel repair --plat manylinux2014_x86_64 dist/awscrt-*cp311*.whl
+auditwheel repair --plat musllinux_1_1_aarch64 dist/awscrt-*cp311*.whl
 
 # Don't need to build wheels for Python 3.12 and later.
 # The 3.11 wheel uses the stable ABI, so it works with newer versions too.
