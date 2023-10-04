d151f5a7906a758101256873b2ee17b8c5e3bfb1
Jonathan M. Henson <henso@amazon.com>
2023-10-04 11:36:34 -0700

    make sure all the python tools are installed.

--- codebuild/test-sys-install-mode.sh
+++ codebuild/test-sys-install-mode.sh
@@ -8,5 +8,6 @@ set -euxo pipefail
 export AWS_CRT_BUILD_STRICT_MODE=OFF
 python -m venv ./crt-build
 source ./crt-build/bin/activate
+pip install setuptools wheel --install-latest
 python -m setup.py install
 python codebuild/test-config-on-failure.py
