7e5d0d19746b86857b69a9f557fdd18e065936c3
Waqar Ahmed Khan <waahm7@gmail.com>
2023-10-26 20:28:25 -0700

    fix instance type

--- continuous-delivery/test-pip-install.py
+++ continuous-delivery/test-pip-install.py
@@ -5,5 +5,5 @@ import awscrt.s3
 
 print('if the next statement does not explode, the pip install was successful')
 print('Is on Ec2 Instance nitro ? {}', awscrt.s3.is_running_on_ec2_nitro())
-print('Instance Type = {}', awscrt.s3.is_running_on_ec2_nitro())
+print('Instance Type = {}', awscrt.s3.get_ec2_instance_type())
 print('Is Optimized for System = {}', awscrt.s3.is_optimized_for_system())
