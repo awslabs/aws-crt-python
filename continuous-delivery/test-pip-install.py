c903dc48ec6731035410a555fcc5f6172699332a
Waqar Ahmed Khan <waahm7@gmail.com>
2023-10-27 16:34:01 -0700

    fix import

--- continuous-delivery/test-pip-install.py
+++ continuous-delivery/test-pip-install.py
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0.
 
-import awscrt.io
+import awscrt.s3
 
 print("if the next statement does not explode, the pip install was successful")
 print(f"Is on Ec2 Instance nitro ? {awscrt.s3.is_running_on_ec2_nitro()}")
