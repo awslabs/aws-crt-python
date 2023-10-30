606af031b74e8a8d875fc3b67ee42f7bfe1ba4fd
Waqar Ahmed Khan <waahm7@gmail.com>
2023-10-30 08:34:27 -0700

    revert test-pip install

--- continuous-delivery/test-pip-install.py
+++ continuous-delivery/test-pip-install.py
@@ -1,8 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0.
 
-import awscrt.s3
+import awscrt.io
 
-print("if the next statement does not explode, the pip install was successful")
-print(f"Instance Type = {awscrt.s3.get_ec2_instance_type()}")
-print(f"Is Optimized for System = {awscrt.s3.is_optimized_for_system()}")
+print('if the next statement does not explode, the pip install was successful') 
+print('Is alpn supported? {}', awscrt.io.is_alpn_available())
