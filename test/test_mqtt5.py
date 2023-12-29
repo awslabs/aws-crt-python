07bb6a84dca6da0e78f2cf1a49243ea14030228c
Alfred Gedeon <alfred2g@hotmail.com>
2023-12-28 23:00:02 -0800

    Fix variable syntax

--- test/test_mqtt5.py
+++ test/test_mqtt5.py
@@ -1027,7 +1027,7 @@ class Mqtt5ClientTest(NativeResourceTest):
         client_id_publisher = create_client_id()
 
         testTopic = "test/MQTT5_Binding_Python_{uuid.uuid4()}"
-        String sharedTopicfilter = "$share/crttest/test/MQTT5_Binding_Python_{uuid.uuid4()}"
+        sharedTopicfilter = "$share/crttest/test/MQTT5_Binding_Python_{uuid.uuid4()}"
 
 
         tls_ctx_options = io.TlsContextOptions.create_client_with_mtls_from_path(
