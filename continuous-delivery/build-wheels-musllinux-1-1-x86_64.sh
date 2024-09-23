d5c0226465b40d8d4db8d5f9e5c2a23809a843e2
Waqar Ahmed Khan <waahm7@gmail.com>
2024-09-23 16:29:14 -0700

    Update continuous-delivery/build-wheels-musllinux-1-1-x86_64.sh
    Co-authored-by: Michael Graeb <graebm@amazon.com>

--- continuous-delivery/build-wheels-musllinux-1-1-x86_64.sh
+++ continuous-delivery/build-wheels-musllinux-1-1-x86_64.sh
@@ -19,7 +19,7 @@ auditwheel repair --plat musllinux_1_1_x86_64 dist/awscrt-*cp310*.whl
 /opt/python/cp311-cp311/bin/python setup.py sdist bdist_wheel
 auditwheel repair --plat musllinux_1_1_x86_64 dist/awscrt-*cp311*.whl
 
-# Don't need to build wheels for Python 3.12 and later.
+# Don't need to build wheels for Python 3.12.
 # The 3.11 wheel uses the stable ABI, so it works with newer versions too.
 
 # We are using the Python 3.13 stable ABI from Python 3.13 onwards because of deprecated functions.
