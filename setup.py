ec21fc399958c86d4b94b64bab09cb65a20ac1a0
Waqar Ahmed Khan <waahm7@gmail.com>
2024-09-26 09:52:12 -0700

    lint

--- setup.py
+++ setup.py
@@ -327,7 +327,8 @@ def awscrt_ext():
     if sys.platform == 'win32':
         # distutils is deprecated in Python 3.10 and removed in 3.12. However, it still works because Python defines a compatibility interface as long as setuptools is installed.
         # We don't have an official alternative for distutils.ccompiler as of September 2024. See: https://github.com/pypa/setuptools/pull/3445
-        # Once that issue is resolved, we can migrate to the official solution. For now, restrict distutils to Windows only, where it's needed.
+        # Once that issue is resolved, we can migrate to the official solution.
+        # For now, restrict distutils to Windows only, where it's needed.
         import distutils.ccompiler
         # the windows apis being used under the hood. Since we're static linking we have to follow the entire chain down
         libraries += ['Secur32', 'Crypt32', 'Advapi32', 'NCrypt', 'BCrypt', 'Kernel32', 'Ws2_32', 'Shlwapi']
