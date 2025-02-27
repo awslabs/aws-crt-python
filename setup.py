7161fdf2b7f4f8b0180c5820177c123484d87e2a
Steve Kim <sbstevek@amazon.com>
2025-02-27 10:41:37 -0800

    only use a specific system version if building windows

--- setup.py
+++ setup.py
@@ -211,7 +211,8 @@ class awscrt_build_ext(setuptools.command.build_ext.build_ext):
         # This is the minimum version of the Windows SDK needed for schannel.h with SCH_CREDENTIALS and 
         # TLS_PARAMETERS. These are required to build Windows Binaries with TLS 1.3 support. This will tell 
         # Jenkins to use this version of the SDK during build.
-        cmake_args.append('-DCMAKE_SYSTEM_VERSION=10.0.17763.0')
+        if sys.platform == 'win32':
+            cmake_args.append('-DCMAKE_SYSTEM_VERSION=10.0.17763.0')
 
         cmake_args.extend(determine_generator_args())
         cmake_args.extend(determine_cross_compile_args())
