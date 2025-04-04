fc289c15158805c7d68a8010606701611c950ed6
Vera Xia <zhvxia@amazon.com>
2025-04-04 15:34:17 -0700

    set env var in setup.py

--- continuous-delivery/build-wheels-osx.sh
+++ continuous-delivery/build-wheels-osx.sh
@@ -5,14 +5,14 @@ set -ex
 
 /Library/Frameworks/Python.framework/Versions/3.9/bin/python3 ./continuous-delivery/update-version.py
 
-MACOSX_DEPLOYMENT_TARGET=10.15 /Library/Frameworks/Python.framework/Versions/3.8/bin/python3 -m build
-MACOSX_DEPLOYMENT_TARGET=10.15 /Library/Frameworks/Python.framework/Versions/3.9/bin/python3 -m build
-MACOSX_DEPLOYMENT_TARGET=10.15 /Library/Frameworks/Python.framework/Versions/3.10/bin/python3 -m build
-MACOSX_DEPLOYMENT_TARGET=10.15 /Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m build
+/Library/Frameworks/Python.framework/Versions/3.8/bin/python3 -m build
+/Library/Frameworks/Python.framework/Versions/3.9/bin/python3 -m build
+/Library/Frameworks/Python.framework/Versions/3.10/bin/python3 -m build
+/Library/Frameworks/Python.framework/Versions/3.11/bin/python3 -m build
 
 # Don't need to build wheels for Python 3.12, it works with the 3.11 stable ABI wheel
 
 # We are using the Python 3.13 stable ABI from Python 3.13 onwards because of deprecated functions.
-MACOSX_DEPLOYMENT_TARGET=10.15 /Library/Frameworks/Python.framework/Versions/3.13/bin/python3 -m build
+/Library/Frameworks/Python.framework/Versions/3.13/bin/python3 -m build
 
 #now you just need to run twine (that's in a different script)
