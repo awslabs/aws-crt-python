778065d43242155732a72bf98e386f1831817577
Steve Kim <sbstevek@amazon.com>
2025-08-14 10:56:15 -0700

    try moving deprecated into its own file to be imported from other files

--- awscrt/mqtt.py
+++ awscrt/mqtt.py
@@ -16,35 +16,36 @@ from awscrt.http import HttpProxyOptions, HttpRequest
 from awscrt.io import ClientBootstrap, ClientTlsContext, SocketOptions
 from dataclasses import dataclass
 from awscrt.mqtt5 import Client as Mqtt5Client
+from awscrt.deprecation import deprecated
 
-# TYPE_CHECKING is used to exclusively execute code by static analysers. Never at runtime.
-from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    # Static analysers will always attempt to import deprecated from typing_extensions and
-    # fall back to known interpretation of `deprecated` if it fails and appropriately handle
-    # the `@deprecated` tags.
-    from typing_extensions import deprecated
-else:
-    _impl = None
-    try:
-        # preferred import of deprecated
-        from typing_extensions import deprecated as _impl
-    except Exception:
-        try:
-            from typing import deprecated as _impl  # Python 3.13+
-        except Exception:
-            _impl = None
+# # TYPE_CHECKING is used to exclusively execute code by static analysers. Never at runtime.
+# from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+#     # Static analysers will always attempt to import deprecated from typing_extensions and
+#     # fall back to known interpretation of `deprecated` if it fails and appropriately handle
+#     # the `@deprecated` tags.
+#     from typing_extensions import deprecated
+# else:
+#     _impl = None
+#     try:
+#         # preferred import of deprecated
+#         from typing_extensions import deprecated as _impl
+#     except Exception:
+#         try:
+#             from typing import deprecated as _impl  # Python 3.13+
+#         except Exception:
+#             _impl = None
 
-    def deprecated(msg=None, *, since=None):
-        if _impl is None:
-            def _noop(obj): return obj
-            return _noop
-        if since is not None:
-            try:
-                return _impl(msg, since=since)
-            except TypeError:
-                pass  # older typing_extensions: no 'since' keyword
-        return _impl(msg)
+#     def deprecated(msg=None, *, since=None):
+#         if _impl is None:
+#             def _noop(obj): return obj
+#             return _noop
+#         if since is not None:
+#             try:
+#                 return _impl(msg, since=since)
+#             except TypeError:
+#                 pass  # older typing_extensions: no 'since' keyword
+#         return _impl(msg)
 
 
 class QoS(IntEnum):
