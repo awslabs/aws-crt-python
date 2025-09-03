01957824ee41575b378a78e2fdb0e87a5a670f63
Dengke Tang <dengket@amazon.com>
2025-09-03 10:34:22 -0700

    make sure it passed down correctly

--- test/test_s3.py
+++ test/test_s3.py
@@ -25,6 +25,7 @@ from awscrt.s3 import (
     S3RequestType,
     S3ResponseError,
     CrossProcessLock,
+    S3FileIoOptions,
     create_default_s3_signing_config,
     get_optimized_platforms,
 )
@@ -397,12 +398,14 @@ class S3RequestTest(NativeResourceTest):
             enable_s3express=False,
             mem_limit=None,
             **kwargs):
+        fio_options = S3FileIoOptions(should_stream=True, disk_throughput_gbps=10.0, direct_io=True)
         s3_client = s3_client_new(
             False,
             self.region,
             self.part_size,
             enable_s3express=enable_s3express,
-            mem_limit=mem_limit)
+            mem_limit=mem_limit,
+            fio_options=fio_options)
         signing_config = None
         if enable_s3express:
             signing_config = AwsSigningConfig(
@@ -589,6 +592,16 @@ class S3RequestTest(NativeResourceTest):
         content_length = os.stat(self.temp_put_obj_file_path).st_size
         request = self._put_object_request(None, content_length)
         self._test_s3_put_get_object(request, S3RequestType.PUT_OBJECT, send_filepath=self.temp_put_obj_file_path)
+
+    def test_put_object_filepath_with_fio_options(self):
+        content_length = os.stat(self.temp_put_obj_file_path).st_size
+        request = self._put_object_request(None, content_length)
+        fio_options = S3FileIoOptions(should_stream=True, disk_throughput_gbps=10.0, direct_io=True)
+        self._test_s3_put_get_object(
+            request,
+            S3RequestType.PUT_OBJECT,
+            send_filepath=self.temp_put_obj_file_path,
+            fio_options=fio_options)
 
     def test_put_object_filepath_unknown_content_length(self):
         content_length = os.stat(self.temp_put_obj_file_path).st_size
