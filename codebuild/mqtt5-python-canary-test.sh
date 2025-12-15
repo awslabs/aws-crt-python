dd8fb7ec025b5c00a62582fcc13b55dbcc2efef7
Bret Ambrose <bambrose@amazon.com>
2025-12-15 13:35:27 -0800

    Canary updates

--- codebuild/mqtt5-python-canary-test.sh
+++ codebuild/mqtt5-python-canary-test.sh
@@ -9,6 +9,5 @@ git submodule update --init
 # build package
 cd $CODEBUILD_SRC_DIR
 
-export AWS_TEST_S3=YES
 python -m pip install --verbose .
-python codebuild/CanaryWrapper.py --canary_executable 'python test/mqtt5_canary.py' --git_hash ${GIT_HASH} --git_repo_name $PACKAGE_NAME --codebuild_log_path $CODEBUILD_LOG_PATH
+python codebuild/CanaryWrapper.py --canary_executable 'python test/mqtt5_canary.py' --git_hash ${GIT_HASH} --git_repo_name $PACKAGE_NAME --codebuild_log_path $CODEBUILD_LOG_PATH --ticket_item "IoT SDK for Python" --cloudwatch_namespace mqtt5_canary
