2cc6f1bd682e3fbabb5cf239d052a016a5471891
Dengke Tang <dengket@amazon.com>
2026-03-24 13:45:24 -0700

    i don't know, but that seems to be the only thing different than it's doc

--- source/module.c
+++ source/module.c
@@ -619,7 +619,13 @@ PyObject *aws_py_weakref_get_ref(PyObject *ref) {
 }
 
 int aws_py_gilstate_ensure(PyGILState_STATE *out_state) {
+    /* If Python >= 3.13 */
+#if PY_VERSION_HEX >= 0x030D0000
+    // Py_IsFinalizing is part of the Stable ABI since version 3.13.
+    if (AWS_LIKELY(Py_IsFinalizing())) {
+#else
     if (AWS_LIKELY(Py_IsInitialized())) {
+#endif
         *out_state = PyGILState_Ensure();
         return AWS_OP_SUCCESS;
     }
