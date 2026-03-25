d9cc6ac1126d212bbef5138ab90e6ec8b607b311
Dengke Tang <dengket@amazon.com>
2026-03-25 16:02:39 -0700

    add prebuild wheels for freethreading

--- continuous-delivery/build-wheels-win64.bat
+++ continuous-delivery/build-wheels-win64.bat
@@ -10,6 +10,10 @@
 :: We are using the 3.13 stable ABI from 3.13 onwards because of deprecated functions.
 "C:\Program Files\Python313\python.exe" -m build || goto error
 
+:: The free-threaded build does not currently support the Limited C API or the stable ABI. Built them separately
+"C:\Program Files\Python-313\python3.13t.exe" -m build || goto error
+"C:\Program Files\Python314\python3.14t.exe" -m build || goto error
+
 goto :EOF
 
 :error
